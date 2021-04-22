051ff8548cdd6ad4cc4f1759d0e8d87f1c22da98
Peter Borissow <peter.borissow@yahoo.com>
2021-04-22 06:37:13 -0400

    Implemented labels pie chart setting in the ChartEditor

--- web/app/ChartEditor.js
+++ web/app/ChartEditor.js
@@ -511,6 +511,10 @@ bluewave.ChartEditor = function(parent, config) {
         var cutout = chartConfig.pieCutout;
         if (cutout==null) cutout = 0.65;
         var innerRadius = radius*cutout;
+        var arc = d3.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(radius);
+
 
         pieArea
             .attr("width", width)
@@ -520,24 +524,91 @@ bluewave.ChartEditor = function(parent, config) {
                 "translate(" + width/2 + "," + height/2 + ")"
             );
 
-        pieArea
+
+        var pieChart = pieArea.append("g");
+
+        pieChart
             .selectAll("whatever")
             .data(pieData)
             .enter()
             .append("path")
-            .attr(
-                "d",
-                d3
-                .arc()
-                .innerRadius(innerRadius)
-                .outerRadius(radius)
-            )
+            .attr("d", arc)
             .attr("fill", function (d) {
                 return getColor(d.data.key);
             })
             .attr("stroke", "#777")
             .style("stroke-width", "1px")
             .style("opacity", 0.7);
+
+
+
+
+        if (chartConfig.pieLabels===true){
+
+          //Another arc that won't be drawn. Just for labels positioning
+            var outerArc = d3
+              .arc()
+              .innerRadius(radius * 0.9)
+              .outerRadius(radius * 0.9);
+
+
+          //Add the polylines between chart and labels:
+            let cPositions = [];
+            var lines = pieArea.append("g");
+            lines
+              .selectAll("allPolylines")
+              .data(pieData)
+              .enter()
+              .append("polyline")
+              .attr("stroke", "black")
+              .style("fill", "none")
+              .attr("stroke-width", 1)
+              .attr("points", function (d) {
+                var posA = arc.centroid(d); // line insertion in the slice
+                var posB = outerArc.centroid(d); // line break: we use the other arc generator that has been built only for that
+                var posC = outerArc.centroid(d); // Label position = almost the same as posB
+                cPositions.forEach((val) => {
+                  if (posC[1] < val + 5 && posC[1] > val - 5) {
+                    posC[1] -= 14;
+                    posB[1] -= 14;
+                  }
+                });
+                cPositions.push(posC[1]);
+                var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2; // we need the angle to see if the X position will be at the extreme right or extreme left
+                posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+                return [posA, posB, posC];
+              });
+
+
+          //Add the polylines between chart and labels:
+            var positions = [];
+            var labels = pieArea.append("g");
+            labels
+              .selectAll("allLabels")
+              .data(pieData)
+              .enter()
+              .append("text")
+              .text(function (d) {
+                  return d.data.key;
+              })
+              .attr("transform", function (d) {
+                var pos = outerArc.centroid(d);
+                positions.forEach((val) => {
+                  if (pos[1] < val + 5 && pos[1] > val - 5) {
+                    pos[1] -= 14;
+                  }
+                });
+                positions.push(pos[1]);
+
+                var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+                pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+                return "translate(" + pos + ")";
+              })
+              .style("text-anchor", function (d) {
+                var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+                return midangle < Math.PI ? "start" : "end";
+              });
+        }
     };
 
 
@@ -929,16 +1000,21 @@ bluewave.ChartEditor = function(parent, config) {
             chartConfig.pieCutout = cutout;
             form.findField("cutout").setValue(cutout*100);
 
+
           //Tweak height of the label field and set initial value
             var labelField = form.findField("labels");
             labelField.row.style.height = "68px";
-            labelField.setValue(false);
+            var labels = chartConfig.pieLabels;
+            labelField.setValue(labels===true ? true : false);
+
 
           //Process onChange events
             form.onChange = function(){
                 var settings = form.getData();
-                console.log(settings);
                 chartConfig.pieCutout = settings.cutout/100;
+                if (settings.labels==="true") settings.labels = true;
+                else if (settings.labels==="false") settings.labels = false;
+                chartConfig.pieLabels = settings.labels;
                 createPiePreview();
             };
         }
