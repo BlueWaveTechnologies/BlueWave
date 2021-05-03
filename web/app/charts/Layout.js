1565ff0cff131143bd660972d1b17b70f8f19c16
Peter Borissow <peter.borissow@yahoo.com>
2021-05-03 12:26:38 -0400

    Interim commit

--- web/app/charts/Layout.js
+++ web/app/charts/Layout.js
@@ -17,6 +17,11 @@ bluewave.charts.Layout = function(parent, config) {
 
         }
     };
+    var titleDiv, body;
+    var layout;
+
+    var width = 350; //360
+    var height = 250; //260
 
 
   //**************************************************************************
@@ -24,16 +29,126 @@ bluewave.charts.Layout = function(parent, config) {
   //**************************************************************************
     var init = function(){
 
+        var div = document.createElement("div");
+        div.style.height = "100%";
+        parent.appendChild(div);
+        me.el = div;
+
+
+      //Create main table
+        var table = createTable();
+        var tbody = table.firstChild;
+        var tr, td;
+
+
+      //Create header nav
+        tr = document.createElement("tr");
+        tbody.appendChild(tr);
+        td = document.createElement("td");
+        tr.appendChild(td);
+        createHeader(td);
+
+
+      //Create body
+        tr = document.createElement("tr");
+        tbody.appendChild(tr);
+        td = document.createElement("td");
+        td.className = "grid-panel";
+        td.style.height = "100%";
+        td.style.padding = "26px";
+        tr.appendChild(td);
+        body = td;
+
+        div.appendChild(table);
+
+
+        layout = document.createElement("div");
+        layout.className = "dashboard-layout";
+        layout.style.width = "100%";
+        layout.style.height = "100%";
+        td.appendChild(layout);
+    };
+
+
+  //**************************************************************************
+  //** clear
+  //**************************************************************************
+    this.clear = function(){
+        layout.innerHTML = "";
     };
 
 
   //**************************************************************************
   //** update
   //**************************************************************************
-    this.update = function(){
+    this.update = function(thumbnails, config){
+        for (var thumbnailID in thumbnails) {
+            if (thumbnails.hasOwnProperty(thumbnailID)){
+                var thumbnail = thumbnails[thumbnailID];
+                var base64image = thumbnail;
+
+                var dashboardItem = createDashboardItem(layout,{
+                    width: width,
+                    height: height,
+                    title: "Hospital Capacity By State",
+                    subtitle: "Ordered By Increased Demand"
+                });
+
+
+                dashboardItem.innerDiv.innerHTML =
+                "<img class='noselect' src='" + base64image + "' style='width:100%'/>";
+
+
+            }
+        }
+
+        layout.style.height = "";
+        onRender(layout, function(){
+
+            layout.style.height = body.offsetHeight + "px";
+            layout.style.minHeight = layout.style.height;
+
+
+            var packery = new Packery(layout, {
+                columnWidth: width,
+                gutter: 25
+            });
+
+
+            for (var i=0; i<packery.items.length; i++){
+                var draggie = new Draggabilly(packery.items[i].element);
+                packery.bindDraggabillyEvents(draggie);
+            }
+
+        });
 
     };
 
+
+  //**************************************************************************
+  //** getConfig
+  //**************************************************************************
+    this.getConfig = function(){
+
+    };
+
+
+  //**************************************************************************
+  //** createHeader
+  //**************************************************************************
+    var createHeader = function(parent){
+        titleDiv = document.createElement("div");
+        titleDiv.className = "dashboard-title noselect";
+        parent.appendChild(titleDiv);
+    };
+
+
+  //**************************************************************************
+  //** Utils
+  //**************************************************************************
+    var onRender = javaxt.dhtml.utils.onRender;
+    var createTable = javaxt.dhtml.utils.createTable;
+    var createDashboardItem = bluewave.utils.createDashboardItem;
 
     init();
 
