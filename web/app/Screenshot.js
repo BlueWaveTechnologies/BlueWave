adc985063212f796632bb62a052ede325d9777ff
Peter Borissow <peter.borissow@yahoo.com>
2021-05-05 17:49:41 -0400

    Updated Screenshot to use the base64ToBlob() method in Utils.js

--- web/app/Screenshot.js
+++ web/app/Screenshot.js
@@ -199,39 +199,11 @@ bluewave.Screenshot = function(parent, config) {
     };
 
 
-
-  //**************************************************************************
-  //** base64ToBlob
-  //**************************************************************************
-    var base64ToBlob = function(base64, mime) {
-
-        mime = mime || '';
-        var sliceSize = 1024;
-        var byteChars = window.atob(base64);
-        var byteArrays = [];
-
-        for (var offset = 0, len = byteChars.length; offset < len; offset += sliceSize) {
-            var slice = byteChars.slice(offset, offset + sliceSize);
-
-            var byteNumbers = new Array(slice.length);
-            for (var i = 0; i < slice.length; i++) {
-                byteNumbers[i] = slice.charCodeAt(i);
-            }
-
-            var byteArray = new Uint8Array(byteNumbers);
-
-            byteArrays.push(byteArray);
-        }
-
-        return new Blob(byteArrays, {type: mime});
-    };
-
-
   //**************************************************************************
   //** Utils
   //**************************************************************************
     var createTable = javaxt.dhtml.utils.createTable;
-    var post = javaxt.dhtml.utils.post;
+    var base64ToBlob = bluewave.utils.base64ToBlob;
 
     init();
 };
