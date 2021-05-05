f768f85eff64e24f02430f573e766cf7ef550694
Peter Borissow <peter.borissow@yahoo.com>
2021-05-05 17:48:51 -0400

    Added base64ToBlob() to Utils.js

--- web/app/Utils.js
+++ web/app/Utils.js
@@ -849,6 +849,33 @@ bluewave.utils = {
 
         //draw
         ctx.putImageData(img2, 0, 0);
+    },
+
+
+  //**************************************************************************
+  //** base64ToBlob
+  //**************************************************************************
+    base64ToBlob: function(base64, mime) {
+
+        mime = mime || '';
+        var sliceSize = 1024;
+        var byteChars = window.atob(base64);
+        var byteArrays = [];
+
+        for (var offset = 0, len = byteChars.length; offset < len; offset += sliceSize) {
+            var slice = byteChars.slice(offset, offset + sliceSize);
+
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            var byteArray = new Uint8Array(byteNumbers);
+
+            byteArrays.push(byteArray);
+        }
+
+        return new Blob(byteArrays, {type: mime});
     }
 
 };
