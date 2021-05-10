c47434bc6859c3073869760c55817e346d94428a
Peter Borissow <peter.borissow@yahoo.com>
2021-05-10 08:39:55 -0400

    Updated ChartEditor to rely on the new PieChart class

--- web/app/ChartEditor.js
+++ web/app/ChartEditor.js
@@ -24,7 +24,7 @@ bluewave.ChartEditor = function(parent, config) {
     var inputData = [];
     var svg;
     var previewArea;
-    var pieArea;
+    var pieChart;
     var plotArea;
     var mapArea;
     var mapLayer;
@@ -169,9 +169,13 @@ bluewave.ChartEditor = function(parent, config) {
         });
     };
 
+
+  //**************************************************************************
+  //** getNode
+  //**************************************************************************
     this.getNode = function() {
         return currentNode;
-    }
+    };
 
 
   //**************************************************************************
@@ -207,7 +211,7 @@ bluewave.ChartEditor = function(parent, config) {
         panel.title.innerHTML = "Untitled";
         optionsDiv.innerHTML = "";
 
-        if (pieArea) pieArea.selectAll("*").remove();
+        if (pieChart) pieChart.clear();
         if (plotArea) plotArea.selectAll("*").remove();
         if (mapArea) mapArea.selectAll("*").remove();
         if (mapLayer) mapLayer.selectAll("circle").remove();
@@ -349,8 +353,8 @@ bluewave.ChartEditor = function(parent, config) {
   //** createPieDropdown
   //**************************************************************************
     var createPieDropdown = function(tbody){
-        dropdownItem(tbody,"pieKey","Key",piePreviewRender,pieInputs,"key");
-        dropdownItem(tbody,"pieValue","Value",piePreviewRender,pieInputs,"value");
+        dropdownItem(tbody,"pieKey","Key",createPiePreview,pieInputs,"key");
+        dropdownItem(tbody,"pieValue","Value",createPiePreview,pieInputs,"value");
     };
 
 
@@ -470,145 +474,24 @@ bluewave.ChartEditor = function(parent, config) {
             );
 
 
-        pieArea = svg.append("g");
+        pieChart = new bluewave.charts.PieChart(svg, {
+            margin: margin
+        });
+
         mapArea = svg.append("g");
         mapLayer = svg.append("g");
     };
 
-  //**************************************************************************
-  //** piePreviewRender
-  //**************************************************************************
-    var piePreviewRender = function(){
-        if(chartConfig.pieKey===null || chartConfig.pieValue===null){ return; }
-        onRender(previewArea, function(){
-            createPiePreview();
-            });
-    }
 
   //**************************************************************************
   //** createPiePreview
   //**************************************************************************
     var createPiePreview = function(){
-        if (pieArea) pieArea.selectAll("*").remove();
-
-        var data = inputData[0];
-        let pieData = data.reduce((acc,curVal)=>{
-            acc[curVal[chartConfig.pieKey]] = curVal[chartConfig.pieValue];
-            return acc;
-        },{});
-
-
-        var pie = d3.pie().value(function (d) {
-            return d.value;
+        if (chartConfig.pieKey===null || chartConfig.pieValue===null) return;
+        onRender(previewArea, function(){
+            var data = inputData[0];
+            pieChart.update(chartConfig, data);
         });
-        pieData = pie(d3.entries(pieData));
-        var width = previewArea.offsetWidth;
-        var height = previewArea.offsetHeight;
-        var radius =
-            Math.min(width, height) / 2 -
-            margin.left -
-            margin.right;
-        radius = radius*1.2;
-
-
-        var cutout = chartConfig.pieCutout;
-        if (cutout==null) cutout = 0.65;
-        var innerRadius = radius*cutout;
-        var arc = d3.arc()
-            .innerRadius(innerRadius)
-            .outerRadius(radius);
-
-        pieArea
-            .attr("width", width)
-            .attr("height", height)
-            .attr(
-                "transform",
-                "translate(" + width/2 + "," + height/2 + ")"
-            );
-        var pieChart = pieArea.append("g");
-
-        pieChart
-            .selectAll("whatever")
-            .data(pieData)
-            .enter()
-            .append("path")
-            .attr("d", arc)
-            .attr("fill", function (d) {
-                return getColor(d.data.key);
-            })
-            .attr("stroke", "#777")
-            .style("stroke-width", "1px")
-            .style("opacity", 0.7);
-
-
-
-        if (chartConfig.pieLabels==null) chartConfig.pieLabels = true;
-        if (chartConfig.pieLabels===true){
-
-          //Another arc that won't be drawn. Just for labels positioning
-            var outerArc = d3
-              .arc()
-              .innerRadius(radius * 0.9)
-              .outerRadius(radius * 0.9);
-
-
-          //Add the polylines between chart and labels:
-            let cPositions = [];
-            var lines = pieArea.append("g");
-            lines
-              .selectAll("allPolylines")
-              .data(pieData)
-              .enter()
-              .append("polyline")
-              .attr("stroke", "black")
-              .style("fill", "none")
-              .attr("stroke-width", 1)
-              .attr("points", function (d) {
-                var posA = arc.centroid(d); // line insertion in the slice
-                var posB = outerArc.centroid(d); // line break: we use the other arc generator that has been built only for that
-                var posC = outerArc.centroid(d); // Label position = almost the same as posB
-                cPositions.forEach((val) => {
-                  if (posC[1] < val + 5 && posC[1] > val - 5) {
-                    posC[1] -= 14;
-                    posB[1] -= 14;
-                  }
-                });
-                cPositions.push(posC[1]);
-                var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2; // we need the angle to see if the X position will be at the extreme right or extreme left
-                posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
-                return [posA, posB, posC];
-              });
-
-
-          //Add the polylines between chart and labels:
-            var positions = [];
-            var labels = pieArea.append("g");
-            labels
-              .selectAll("allLabels")
-              .data(pieData)
-              .enter()
-              .append("text")
-              .text(function (d) {
-                  return d.data.key;
-              })
-              .attr("transform", function (d) {
-                var pos = outerArc.centroid(d);
-                positions.forEach((val) => {
-                  if (pos[1] < val + 5 && pos[1] > val - 5) {
-                    pos[1] -= 14;
-                  }
-                });
-                positions.push(pos[1]);
-
-                var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-                pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
-                return "translate(" + pos + ")";
-              })
-              .style("text-anchor", function (d) {
-                var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-                return midangle < Math.PI ? "start" : "end";
-              });
-        }
     };
 
 
