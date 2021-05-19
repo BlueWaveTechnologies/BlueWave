2cff56132394158c209352017dc696414de8cf12
Peter Borissow <peter.borissow@yahoo.com>
2021-05-19 06:31:54 -0400

    Initial commit

---
+++ src/main/java/bluewave/utils/NotificationService.java
@@ -0,0 +1,125 @@
+package bluewave.utils;
+import java.util.*;
+
+
+public class NotificationService {
+
+    private static List events;
+    private static List<Listener> listeners;
+
+
+  //**************************************************************************
+  //** notify
+  //**************************************************************************
+    public static void notify(String event, String data){
+        if (events==null) return;
+
+
+        synchronized(events){
+            events.add(new Object[]{event, data});
+            events.notify();
+        }
+    }
+
+
+  //**************************************************************************
+  //** start
+  //**************************************************************************
+    public static void start(){
+        listeners = new LinkedList();
+
+        events = new LinkedList();
+        int numThreads = 4;
+        for (int i=0; i<numThreads; i++){
+            Thread thread = new Thread(new NotificationProcessor());
+            thread.start();
+        }
+    }
+
+
+  //**************************************************************************
+  //** stop
+  //**************************************************************************
+    public static void stop(){
+
+        synchronized(listeners){
+            listeners.clear();
+            listeners.notify();
+        }
+
+        synchronized(events){
+            events.clear();
+            events.add(0, null);
+            events.notify();
+        }
+    }
+
+
+  //**************************************************************************
+  //** addListener
+  //**************************************************************************
+    public static void addListener(Listener listener){
+        synchronized(listeners){
+            listeners.add(listener);
+            listeners.notify();
+        }
+    }
+
+    
+  //**************************************************************************
+  //** Listener Class
+  //**************************************************************************
+    public static class Listener {
+        public void processEvent(String event, String data){}
+    }
+
+
+  //**************************************************************************
+  //** NotificationProcessor
+  //**************************************************************************
+  /** Thread used to pass events to listeners
+   */
+    private static class NotificationProcessor implements Runnable {
+
+        public void run() {
+
+            while (true) {
+
+                Object obj = null;
+                synchronized (events) {
+                    while (events.isEmpty()) {
+                        try {
+                            events.wait();
+                        }
+                        catch (InterruptedException e) {
+                            return;
+                        }
+                    }
+                    obj = events.get(0);
+                    if (obj!=null) events.remove(0);
+                    events.notifyAll();
+                }
+
+                if (obj!=null){
+                    Object[] arr = (Object[]) obj;
+                    String event = (String) arr[0];
+                    String data = (String) arr[1];
+
+
+                    for (Listener listener : listeners){
+                        try{
+                            listener.processEvent(event, data);
+                        }
+                        catch(Exception e){
+                        }
+                    }
+
+                }
+                else{
+                    return;
+                }
+            }
+        }
+    }
+
+}
