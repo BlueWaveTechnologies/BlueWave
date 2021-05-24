62a10f1fce2c89c629e1f4209c3a1cd477009081
Peter Borissow <peter.borissow@yahoo.com>
2021-05-24 17:25:55 -0400

    Implemented getCompanies() method in SupplyChainService

--- src/main/java/bluewave/web/services/SupplyChainService.java
+++ src/main/java/bluewave/web/services/SupplyChainService.java
@@ -8,7 +8,13 @@ import javaxt.http.servlet.ServletException;
 import javaxt.sql.Database;
 
 import java.io.IOException;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Value;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class SupplyChainService extends WebService {
 
@@ -27,7 +33,57 @@ public class SupplyChainService extends WebService {
   //**************************************************************************
     public ServiceResponse getCompanies(ServiceRequest request, Database database)
         throws ServletException, IOException {
-        return new ServiceResponse(501, "Not Implemented");
+
+        String name = request.getParameter("name").toString();
+        if (name==null) return new ServiceResponse(400, "Name is required");
+
+        Long limit = request.getParameter("limit").toLong();
+        if (limit==null || limit<1 || limit>50) limit = 10L;
+
+        String query = "MATCH (n:registrationlisting_registration)\n" +
+        "WHERE n.name STARTS WITH '" + name + "'\n" +
+        "RETURN ID(n) as id, properties(n) as registrationlisting_registration " +
+        "ORDER BY n.name\n" +
+        "LIMIT " + limit;
+
+
+        Session session = null;
+        try {
+            session = graph.getSession();
+
+            StringBuilder str = new StringBuilder("[");
+
+            Result rs = session.run(query);
+            while (rs.hasNext()){
+                str.append("{");
+                Record record = rs.next();
+                str.append("\"id\":");
+                str.append(record.get("id"));
+                str.append(",");
+
+                Value val = record.get("registrationlisting_registration");
+                if (!val.isNull()){
+                    Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+                    String json = gson.toJson(val.asMap());
+                    str.append("\"company\":");
+                    str.append(json);
+                }
+                str.append("}");
+                if (rs.hasNext()) str.append(",");
+            }
+            session.close();
+
+            str.append("]");
+
+          //Return response
+            ServiceResponse response = new ServiceResponse(str.toString());
+            response.setContentType("application/json");
+            return response;
+        }
+        catch (Exception e) {
+            if (session != null) session.close();
+            return new ServiceResponse(e);
+        }
     }
 
 
