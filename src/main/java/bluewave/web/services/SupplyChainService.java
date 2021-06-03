2de40912f619c495c215756e6226c889645a63b8
Peter Borissow <peter.borissow@yahoo.com>
2021-06-03 17:58:28 -0400

    Added new getProducts() saveCompany() and saveFacility() methods to the SupplyChainService

--- src/main/java/bluewave/web/services/SupplyChainService.java
+++ src/main/java/bluewave/web/services/SupplyChainService.java
@@ -193,7 +193,103 @@ public class SupplyChainService extends WebService {
   //**************************************************************************
     public ServiceResponse saveCompany(ServiceRequest request, Database database)
         throws ServletException, IOException {
-        return new ServiceResponse(501, "Not Implemented");
+
+      //Get user associated with the request
+        bluewave.app.User user = (bluewave.app.User) request.getUser();
+
+
+      //Prevent read-only users from saving
+        if (user.getAccessLevel()<3) throw new ServletException(403, "Not Authorized");
+
+
+      //Parse payload
+        JSONObject json = request.getJson();
+        Long companyID = json.get("id").toLong();
+        String companyName = json.get("name").toString();
+        Long sourceID = json.get("sourceID").toLong(); //owner_operator_number
+
+
+
+      //Create or update company
+        Session session = null;
+        try{
+            session = graph.getSession();
+
+
+            if (companyID==null){
+
+                List<String> properties = new ArrayList<>();
+                properties.add("name: '" + companyName + "'");
+                if (sourceID!=null) properties.add("sourceID: '" + sourceID + "'");
+
+
+                String query = "CREATE (a:company {" + String.join(", ", properties) + "}) RETURN id(a)";
+                Result result = session.run(query);
+                if (result.hasNext()) {
+                    companyID = result.single().get(0).asLong();
+                }
+
+                if (sourceID!=null){
+                    //link company node to registrationlisting_registration_owner_operator?
+                }
+
+            }
+            else{
+                if (sourceID==null){
+                    //update company
+
+                    HashMap<String, Object> updates = new HashMap<>();
+
+                    String query = "MATCH (n:company)\n" +
+                    "WHERE id(n) = " + companyID + "\n" +
+                    "RETURN properties(n) as company";
+                    Result rs = session.run(query);
+                    if (rs.hasNext()){
+                        Record record = rs.next();
+                        Value val = record.get("company");
+                        if (!val.isNull()){
+                            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+                            JSONObject company = new JSONObject(gson.toJson(val.asMap()));
+                            String name = company.get("name").toString();
+                            if (companyName!=null){
+                                companyName = companyName.trim();
+                                if (!companyName.equals(name)){
+                                    updates.put("name", companyName);
+                                }
+                            }
+                        }
+                    }
+
+                    if (!updates.isEmpty()){
+                        StringBuilder stmt = new StringBuilder();
+                        stmt.append("MATCH (n:company) WHERE id(n) = " + companyID + "\n");
+                        Iterator<String> it = updates.keySet().iterator();
+                        while (it.hasNext()){
+                            String key = it.next();
+                            Object val = updates.get(key);
+                            stmt.append("SET n." + key + " = '" + val + "'\n");
+                        }
+                        Result result = session.run(query);
+                        if (result.hasNext()) {
+                            companyID = result.single().get(0).asLong();
+                        }
+                    }
+
+                }
+                else{
+                    //update registrationlisting and company?
+                    throw new Exception("Not Implemented");
+                }
+            }
+
+            session.close();
+
+            return new ServiceResponse(200, companyID+"");
+        }
+        catch (Exception e) {
+            if (session != null) session.close();
+            return new ServiceResponse(e);
+        }
     }
 
 
@@ -210,7 +306,40 @@ public class SupplyChainService extends WebService {
         Long ownerOperatorID = request.getParameter("owner_operator_number").toLong();
 
         if (companyID!=null){
+                JSONArray arr = new JSONArray();
 
+                String query = "MATCH (n:facility)\n" +
+                "WHERE n.companyID = " + companyID + "\n" +
+                "RETURN id(n) as id, properties(n) as facility";
+
+                Session session = null;
+                try{
+                    session = graph.getSession();
+                    Result rs = session.run(query);
+                    while (rs.hasNext()){
+                        Record record = rs.next();
+                        long nodeID = record.get("id").asLong();
+                        JSONObject json = new JSONObject();
+
+
+                        Value val = record.get("facility");
+                        if (!val.isNull()){
+                            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+                            json = new JSONObject(gson.toJson(val.asMap()));
+                        }
+
+
+                        json.set("id", nodeID);
+                        arr.add(json);
+                    }
+                    session.close();
+                }
+                catch (Exception e) {
+                    if (session != null) session.close();
+                    return new ServiceResponse(e);
+                }
+
+                return new ServiceResponse(arr);
         }
         else{
             if (ownerOperatorID!=null){
@@ -257,6 +386,171 @@ public class SupplyChainService extends WebService {
             }
             else{
                 return new ServiceResponse(400, "companyID or owner_operator_number is required");
+            }
+        }
+    }
+
+
+  //**************************************************************************
+  //** saveFacility
+  //**************************************************************************
+    public ServiceResponse saveFacility(ServiceRequest request, Database database)
+        throws ServletException, IOException {
+
+      //Get user associated with the request
+        bluewave.app.User user = (bluewave.app.User) request.getUser();
+
+
+      //Prevent read-only users from saving
+        if (user.getAccessLevel()<3) throw new ServletException(403, "Not Authorized");
+
+
+      //Parse payload
+        JSONObject json = request.getJson();
+        Long facilityID = json.get("id").toLong();
+        Long companyID = json.get("companyID").toLong(); //company node id
+        String facilityName = json.get("name").toString();
+        Long sourceID = json.get("sourceID").toLong(); //fei_number
+
+
+
+      //Create or update company
+        Session session = null;
+        try{
+            session = graph.getSession();
+
+
+            if (facilityID==null){
+
+                List<String> properties = new ArrayList<>();
+                properties.add("name: '" + facilityName + "'");
+                properties.add("companyID: '" + companyID + "'");
+                if (sourceID!=null) properties.add("sourceID: '" + sourceID + "'");
+
+
+
+                String query = "CREATE (a:facility {" + String.join(", ", properties) + "}) RETURN id(a)";
+                Result result = session.run(query);
+                if (result.hasNext()) {
+                    facilityID = result.single().get(0).asLong();
+                }
+
+                if (sourceID!=null){
+                    //link company node to registrationlisting_registration_owner_operator?
+                }
+
+            }
+            else{
+                if (sourceID==null){
+                    //update company
+
+                    String query = "MATCH (n:facility)\n" +
+                    "WHERE id(n) = " + companyID + "\n" +
+                    "RETURN properties(n)";
+
+
+                }
+                else{
+                    //update registrationlisting and company?
+                    throw new Exception("Not Implemented");
+                }
+            }
+
+            session.close();
+
+            return new ServiceResponse(200, facilityID+"");
+
+        }
+        catch (Exception e) {
+            if (session != null) session.close();
+            return new ServiceResponse(e);
+        }
+    }
+
+
+  //**************************************************************************
+  //** getProducts
+  //**************************************************************************
+  /** Returns a list of products associated with a given facility. Searches
+   *  both "product" nodes and "registrationlisting" nodes
+   */
+    public ServiceResponse getProducts(ServiceRequest request, Database database)
+        throws ServletException, IOException {
+
+        Long facilityID = request.getParameter("facilityID").toLong();
+        Long fei = request.getParameter("fei").toLong();
+
+        if (facilityID!=null){
+
+        }
+        else{
+            if (fei!=null){
+                JSONArray arr = new JSONArray();
+
+                String query =
+                "MATCH (registrationlisting_registration)<-[:has]-(registrationlisting)-[:has]->(registrationlisting_product)-[:has]->(registrationlisting_product_openfda)\n" +
+                "WHERE registrationlisting_registration.fei_number = '" + fei + "'\n" +
+                "RETURN id(registrationlisting_product) as id," +
+                "properties(registrationlisting) as registration, " +
+                "properties(registrationlisting_product) as product, " +
+                "properties(registrationlisting_product_openfda) as product_description";
+
+                Session session = null;
+                try{
+                    session = graph.getSession();
+
+                    Result rs = session.run(query);
+                    while (rs.hasNext()){
+                        Record record = rs.next();
+                        long nodeID = record.get("id").asLong();
+                        JSONObject json = new JSONObject();
+
+
+                        Value val = record.get("product");
+                        if (!val.isNull()){
+                            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+                            json = new JSONObject(gson.toJson(val.asMap()));
+                        }
+
+                        val = record.get("product_description");
+                        if (!val.isNull()){
+                            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+                            JSONObject productInfo = new JSONObject(gson.toJson(val.asMap()));
+                            Iterator<String> it = productInfo.keySet().iterator();
+                            while (it.hasNext()){
+                                String key = it.next();
+                                json.set(key, productInfo.get(key));
+                            }
+                        }
+
+                        val = record.get("registration");
+                        if (!val.isNull()){
+                            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+                            JSONObject productInfo = new JSONObject(gson.toJson(val.asMap()));
+                            Iterator<String> it = productInfo.keySet().iterator();
+                            while (it.hasNext()){
+                                String key = it.next();
+                                json.set(key, productInfo.get(key));
+                            }
+                        }
+
+
+                        json.set("id", nodeID);
+                        arr.add(json);
+                    }
+
+                    session.close();
+                }
+                catch (Exception e) {
+                    if (session != null) session.close();
+                    return new ServiceResponse(e);
+                }
+
+                return new ServiceResponse(arr);
+
+            }
+            else{
+                return new ServiceResponse(400, "facilityID or fei is required");
             }
         }
 
