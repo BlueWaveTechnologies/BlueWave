682e951c782e507515a41667d6503ad77f637f82
Peter Borissow <peter.borissow@yahoo.com>
2021-06-28 09:27:08 -0400

    Added groups to the Homepage

--- web/app/Homepage.js
+++ web/app/Homepage.js
@@ -49,6 +49,11 @@ bluewave.Homepage = function(parent, config) {
         dashboards.addEventListener("remove", function(dashboard){
             refresh();
         }, me);
+
+
+
+
+
     };
 
 
@@ -87,9 +92,85 @@ bluewave.Homepage = function(parent, config) {
     var refresh = function(){
         mainDiv.innerHTML = "";
         var dashboards = config.dataStores["Dashboard"];
-        for (var i=0; i<dashboards.length; i++){
-            var dashboard = dashboards.get(i);
-            add(dashboard);
+        var groups = config.dataStores["DashboardGroup"];
+
+        if (dashboards && groups){
+            render();
+        }
+        else{
+            get("dashboard/groups",{
+                success: function(groups) {
+                    groups = new javaxt.dhtml.DataStore(groups);
+                    config.dataStores["DashboardGroup"] = groups;
+                    render();
+                }
+            });
+        }
+    };
+
+
+  //**************************************************************************
+  //** render
+  //**************************************************************************
+    var render = function(){
+        var dashboards = config.dataStores["Dashboard"];
+        var groups = config.dataStores["DashboardGroup"];
+
+
+      //Create groups as needed
+        if (groups.length===0){
+            var myDashboards = [];
+            var sharedDashboards = [];
+            for (var i=0; i<dashboards.length; i++){
+                var dashboard = dashboards.get(i);
+                if (!dashboard.className || !dashboard.app){
+                    myDashboards.push(dashboard.id);
+                }
+                else{
+                    sharedDashboards.push(dashboard.id);
+                }
+            }
+            if (myDashboards.length>0){
+                groups = new javaxt.dhtml.DataStore();
+                groups.add({
+                    name: "My Dashboards",
+                    dashboards: myDashboards
+                });
+                groups.add({
+                    name: "Shared Dashboards",
+                    dashboards: sharedDashboards
+                });
+            }
+        }
+
+
+      //Render dashboards by group
+        if (groups.length===0){
+            for (var i=0; i<dashboards.length; i++){
+                var dashboard = dashboards.get(i);
+                add(dashboard, mainDiv);
+            }
+        }
+        else{
+            for (var i=0; i<groups.length; i++){
+                var group = groups.get(i);
+                var arr = [];
+                if (group.dashboards){
+                    for (var j=0; j<group.dashboards.length; j++){
+                        var dashboardID = group.dashboards[j];
+                        for (var k=0; k<dashboards.length; k++){
+                            var dashboard = dashboards.get(k);
+                            if (dashboard.id===dashboardID){
+                                arr.push(dashboard);
+                            }
+                        }
+                    }
+                }
+                var g = createGroupBox(group);
+                for (var j=0; j<arr.length; j++){
+                    add(arr[j], g);
+                }
+            }
         }
     };
 
@@ -97,11 +178,11 @@ bluewave.Homepage = function(parent, config) {
   //**************************************************************************
   //** add
   //**************************************************************************
-    var add = function(dashboard){
+    var add = function(dashboard, parent){
         var title = dashboard.name;
 
 
-        var dashboardItem = createDashboardItem(mainDiv, {
+        var dashboardItem = createDashboardItem(parent, {
             width: 360,
             height: 230,
             subtitle: title
@@ -132,8 +213,28 @@ bluewave.Homepage = function(parent, config) {
 
 
   //**************************************************************************
+  //** createGroupBox
+  //**************************************************************************
+    var createGroupBox = function(group){
+        var div = document.createElement("div");
+        div.className = "dashboard-group";
+        div.style.position = "relative";
+
+        var label = document.createElement("div");
+        label.className = "dashboard-group-label";
+        label.style.position = "absolute";
+        label.innerHTML = group.name;
+        div.appendChild(label);
+
+        mainDiv.appendChild(div);
+        return div;
+    };
+
+
+  //**************************************************************************
   //** Utils
   //**************************************************************************
+    var get = bluewave.utils.get;
     var createDashboardItem = bluewave.utils.createDashboardItem;
 
     init();
