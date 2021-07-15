5ee96bec31404ce5c6226ca013b282ccfe7c0578
JohnKim <john.kim@culmen.com>
2021-07-15 06:12:12 -0400

    Tooltip added to scatter plot. Need to decide type of info to be displayed

--- web/app/charts/ScatterChart.js
+++ web/app/charts/ScatterChart.js
@@ -124,6 +124,35 @@ bluewave.charts.ScatterChart = function(parent, config) {
 
                displayAxis("key","value",scatterData);
 
+               var tooltip = d3.select(parent)
+                 .append("div")
+                 .style("opacity", 0)
+                 .attr("class", "tooltip")
+                 .style("background-color", "white")
+                 .style("border", "solid")
+                 .style("border-width", "1px")
+                 .style("border-radius", "5px")
+                 .style("padding", "10px")
+
+               var mouseover = function(d) {
+                  tooltip
+                  .style("opacity", 1)
+               }
+
+               var mousemove = function(d) {
+                  tooltip
+                  .html("Example tooltip: " + "some value entered")
+                  .style("left", (d3.mouse(this)[0]+90) + "px")
+                  .style("top", (d3.mouse(this)[1]) + "px")
+               }
+
+               var mouseleave = function(d) {
+                  tooltip
+                  .transition()
+                  .duration(200)
+                  .style("opacity", 0)
+               }
+
                let keyType = typeOfAxisValue(scatterData[0].key);
                scatterArea
                    .selectAll("dot")
@@ -140,7 +169,10 @@ bluewave.charts.ScatterChart = function(parent, config) {
                       .attr("r", 7)
                       .style("fill", "#12b84c")
                       .style("opacity", 0.3)
-                      .style("stroke", "white");
+                      .style("stroke", "white")
+                      .on("mouseover", mouseover)
+                      .on("mousemove", mousemove)
+                      .on("mouseleave", mouseleave);
 
         });
     };
