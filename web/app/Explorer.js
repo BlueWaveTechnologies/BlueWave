d8c958f0a1d09327295de0659fcff4804915474d
Peter <peter.borissow@yahoo.com>
2021-08-02 18:09:10 -0400

    Updated Explorer to allow users to link SupplyChain to a SankeyChart

--- web/app/Explorer.js
+++ web/app/Explorer.js
@@ -763,13 +763,25 @@ bluewave.Explorer = function(parent, config) {
                     return;
                 }
             }
-            //Ensure that Map can only be connected by addData data type.
-            if(node.type === "map"){
-                if(inputNode.type != "addData" && inputNode.type != "supplyChain"){
+            
+            
+          //Ensure that Map can only be connected by addData or supplyChain nodes
+            if (node.type === "map"){
+                if (inputNode.type != "addData" && inputNode.type != "supplyChain"){
                    drawflow.removeSingleConnection(info.output_id, info.input_id, info.output_class, info.input_class);
                    return;
                 }
             }
+            
+            
+          //Ensure that Sankey can only be connected by a supplyChain node
+            if (node.type === "sankeyChart"){
+                if (inputNode.type != "supplyChain"){
+                   drawflow.removeSingleConnection(info.output_id, info.input_id, info.output_class, info.input_class);
+                   return;
+                }
+            }
+            
 
           //If we're still here, update node and open editor
             node.inputs[outputID] = inputNode;
@@ -1004,7 +1016,6 @@ bluewave.Explorer = function(parent, config) {
                 });
 
                 break;
-            case "sankeyChart":
             case "supplyChain":
 
                 var node = createNode({
@@ -1403,6 +1414,7 @@ bluewave.Explorer = function(parent, config) {
         }
     };
 
+
   //**************************************************************************
   //** editMap
   //**************************************************************************
@@ -1447,10 +1459,11 @@ bluewave.Explorer = function(parent, config) {
 
             mapEditor.hide = function(){
                 win.hide();
-            }
+            };
         }
 
-        //Add custom getNode() method to the mapEditor to return current node
+
+      //Add custom getNode() method to the mapEditor to return current node
         mapEditor.getNode = function(){
             return node;
         };
@@ -1470,8 +1483,7 @@ bluewave.Explorer = function(parent, config) {
         }
         mapEditor.update(node.config, data);
         mapEditor.show();
-
-    }
+    };
 
 
   //**************************************************************************
@@ -1527,14 +1539,31 @@ bluewave.Explorer = function(parent, config) {
             return node;
         };
 
-        sankeyEditor.update(node.config);
+
+        
+      //Special for supply chain inputs
+        var inputs = [];
+        for (var key in node.inputs) {
+            if (node.inputs.hasOwnProperty(key)){
+                var inputNode = node.inputs[key];
+                if (inputNode.type==="supplyChain"){
+                    var inputConfig = inputNode.config;
+                    if (inputConfig) inputs.push(inputConfig);
+                }
+
+            }
+        }
+        
+
+
+        sankeyEditor.update(node.config, inputs);
         sankeyEditor.show();
     };
+
 
   //**************************************************************************
   //** editScatter
   //**************************************************************************
-
     var editScatter = function(node){
         if (!scatterEditor){
             var win = createNodeEditor({
