9997d0edb4ac0856358dacabed2b9194805e4c37
Peter Borissow <peter.borissow@yahoo.com>
2021-09-07 14:00:53 -0400

    Updated field names in the return statement in Nodes_And_Properties.sql

--- src/main/java/bluewave/queries/Nodes_And_Properties.sql
+++ src/main/java/bluewave/queries/Nodes_And_Properties.sql
@@ -2,5 +2,5 @@ MATCH(n)
 WITH LABELS(n) AS labels , KEYS(n) AS keys
 UNWIND labels AS label
 UNWIND keys AS key
-RETURN DISTINCT label, COLLECT(DISTINCT key) AS props
+RETURN DISTINCT label as node, COLLECT(DISTINCT key) AS properties
 ORDER BY label
