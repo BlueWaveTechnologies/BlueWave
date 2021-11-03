082f37d6baf996683b0ed60a75fb70d98ea16019
Jordan.Dobson <jordan.dobson@culmen.com>
2021-11-03 09:12:40 -0400

    feat 185 - Work on The Grid Added in the Thumbnail code.

--- web/app/admin/MapAdmin.js
+++ web/app/admin/MapAdmin.js
@@ -16,7 +16,8 @@ bluewave.MapAdmin = function(parent, config) {
     };
     var waitmask;
     var basemaps = [];
-    var grid, editor;
+    var editor;
+    var addButton, editButton, deleteButton, moveUpButton, moveDownButton;
 
 
   //**************************************************************************
@@ -44,7 +45,6 @@ bluewave.MapAdmin = function(parent, config) {
         tr.appendChild(td);
         createToolbar(td);
 
-
       //Row 2
         tr = document.createElement("tr");
         tbody.appendChild(tr);
@@ -52,7 +52,6 @@ bluewave.MapAdmin = function(parent, config) {
         td.style.height = "100%";
         tr.appendChild(td);
         createBody(td);
-
 
         parent.appendChild(table);
         me.el = table;
@@ -65,6 +64,7 @@ bluewave.MapAdmin = function(parent, config) {
   //**************************************************************************
     this.clear = function(){
         basemaps = [];
+        grid.update();
     };
 
 
@@ -77,8 +77,8 @@ bluewave.MapAdmin = function(parent, config) {
         waitmask.show(500);
         get("admin/settings/basemap", {
             success: function(arr){
-                basemaps = arr;
-               //grid.update();
+               basemaps = arr;
+               grid.update
                waitmask.hide();
             },
             failure: function(request){
@@ -93,7 +93,75 @@ bluewave.MapAdmin = function(parent, config) {
   //** createToolbar
   //**************************************************************************
     var createToolbar = function(parent){
+        var toolbar = document.createElement('div');
 
+          //Add button
+            addButton = createButton(toolbar, {
+                label: "Add",
+                icon: "fas fa-plus-circle"
+            });
+            addButton.onClick = function(){
+                editBaseMap();
+            };
+
+          //Edit button
+            editButton = createButton(toolbar, {
+                label: "Edit",
+                icon: "fas fa-edit",
+                disabled: true
+            });
+            editButton.onClick = function(){
+                var records = grid.getSelectedRecords();
+                if (records.length>0) editBaseMap(records[0]);
+            };
+
+          //Delete button
+            deleteButton = createButton(toolbar, {
+                label: "Delete",
+                icon: "fas fa-trash",
+                disabled: true
+            });
+            deleteButton.onClick = function(){
+                var records = grid.getSelectedRecords();
+                if (records.length>0) deleteBaseMap(records[0]);
+            };
+
+          //MoveUp button
+            moveUpButton = createButton(toolbar, {
+                label: "Move Up",
+                icon: "fas fa-chevron-up",
+                disabled: true
+            });
+            moveUpButton.onClick = function(){
+                var records = grid.getSelectedRecords();
+                //TODO Move this record up one spot.
+            };
+
+          //MoveDown button
+            moveDownButton = createButton(toolbar, {
+                label: "Move Down",
+                icon: "fas fa-chevron-down",
+                disabled: true
+            });
+            moveDownButton.onClick = function(){
+                var records = grid.getSelectedRecords();
+                //TODO Move this record down one spot.
+            };
+
+            createSpacer(toolbar);
+
+          //Refresh button
+            var refreshButton = createButton(toolbar, {
+                label: "Refresh",
+                icon: "fas fa-sync-alt",
+                disabled: false,
+                hidden: false
+            });
+            refreshButton.onClick = function(){
+                grid.update();
+            };
+
+            parent.appendChild(toolbar);
     };
 
 
@@ -102,17 +170,79 @@ bluewave.MapAdmin = function(parent, config) {
   //**************************************************************************
     var createBody = function(parent){
 
+        grid = new javaxt.dhtml.DataGrid(parent, {
+            style: config.style.table,
+            url: "admin/settings/basemap",
+            filter: filter,
+            getResponse: function(url, payload, callback){
+                get(url, {
+                    payload: payload,
+                    success: function(json){
+                        callback.apply(grid, [{
+                           status: 200,
+                           rows: json
+                        }]);
+                    },
+                    failure: function(request){
+                        callback.apply(grid, [request]);
+                    }
+                });
+            },
+            parseResponse: function(obj){
+                return obj.rows;
+            },
+            columns: [
+                {header: 'Name', width:'100%', field:'name'},
+                {header: 'URL', width:'150', field:'url'},
+                {header: 'Key', width:'240', field:'key'},
+                {header: 'Thumbnail', width:'240', field:'thumbnail'}
+            ],
+            update: function(row, basemap){
+                var baseURL = basemap.url;
+                var layer = new ol.layer.Tile({
+                    source: new ol.source.XYZ({
+                        url: baseURL
+                    })
+                });
+
+                var img = document.createElement("img");
+                img.src = preview;
+
+                row.set('Name', basemap.name);
+                row.set('URL', basemap.url);
+                row.set('Key', basemap.key);
+                row.set('Thumbnail', img);
+            }
+        });
+
+        grid.onSelectionChange = function(){
+             var records = grid.getSelectedRecords();
+             if (records.length>0){
+                 editButton.enable();
+                 deleteButton.enable();
+             }
+             else{
+                 editButton.disable();
+                 deleteButton.disable();
+             }
+        };
+
+        grid.update = function(){
+            grid.clear();
+            grid.load();
+        };
+
     };
 
 
   //**************************************************************************
-  //** deleteUser
+  //** updateConfig
   //**************************************************************************
     var updateConfig = function(){
         save("admin/settings/basemap", JSON.stringify(basemaps), {
             success: function(){
-                //editor.close();
-                //grid.update();
+                editor.close();
+                grid.update();
             },
             failure: function(request){
                 alert(request);
@@ -126,6 +256,7 @@ bluewave.MapAdmin = function(parent, config) {
   //**************************************************************************
     var merge = javaxt.dhtml.utils.merge;
     var createTable = javaxt.dhtml.utils.createTable;
+    var createSpacer = bluewave.utils.createSpacer;
     var addShowHide = javaxt.dhtml.utils.addShowHide;
     var get = bluewave.utils.get;
     var save = javaxt.dhtml.utils.post;
