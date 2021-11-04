c2c4472e9f30e8ebb418fba0a6cf3c6356689af3
Peter Borissow <peter.borissow@yahoo.com>
2021-11-04 08:06:24 -0400

    Implemented file upload in the DocumentationService

--- src/main/java/bluewave/web/services/DocumentService.java
+++ src/main/java/bluewave/web/services/DocumentService.java
@@ -1,8 +1,13 @@
 package bluewave.web.services;
-import javaxt.express.*;
+import bluewave.Config;
+import java.io.FileOutputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import javaxt.http.servlet.ServletException;
 import javaxt.http.servlet.FormInput;
 import javaxt.http.servlet.FormValue;
+import javaxt.express.*;
 import javaxt.sql.*;
 import javaxt.json.*;
 
@@ -24,7 +29,7 @@ public class DocumentService extends WebService {
 
 
   //**************************************************************************
-  //** getSettings
+  //** uploadFile
   //**************************************************************************
     private ServiceResponse uploadFile(ServiceRequest request, bluewave.app.User user)
         throws ServletException {
@@ -38,22 +43,82 @@ public class DocumentService extends WebService {
                 String name = input.getName();
                 FormValue value = input.getValue();
                 if (input.isFile()){
-                    console.log(name);
-                    //images.put(name, new javaxt.io.Image(value.getInputStream()));
                     JSONObject json = new JSONObject();
+                    json.set("name", name);
+
+                    javaxt.io.File file = getFile(name, user);
+                    if (!file.exists()){
+                        try{
+                            int bufferSize = 2048;
+                            FileOutputStream output = new FileOutputStream(file.toFile());
+                            final ReadableByteChannel inputChannel = Channels.newChannel(value.getInputStream());
+                            final WritableByteChannel outputChannel = Channels.newChannel(output);
+                            final java.nio.ByteBuffer buffer = java.nio.ByteBuffer.allocateDirect(bufferSize);
+                            int ttl = 0;
+
+                            while (inputChannel.read(buffer) != -1) {
+                                buffer.flip();
+                                ttl+=outputChannel.write(buffer);
+                                buffer.compact();
+                            }
+                            buffer.flip();
+                            while (buffer.hasRemaining()) {
+                                ttl+=outputChannel.write(buffer);
+                            }
+
+                            //console.log(ttl);
+
+                            inputChannel.close();
+                            outputChannel.close();
+
+                            json.set("result", "uploaded");
+                        }
+                        catch(Exception e){
+                            json.set("result", "error");
+                        }
+                    }
+                    else{
+                        json.set("result", "exists");
+                    }
+
+
                     results.add(json);
                 }
             }
-
-
             return new ServiceResponse(results);
-
         }
         catch(Exception e){
             return new ServiceResponse(e);
         }
+    }
 
 
+  //**************************************************************************
+  //** getFile
+  //**************************************************************************
+    private static javaxt.io.File getFile(String name, bluewave.app.User user){
+        return new javaxt.io.File(getUploadDir(), name);
+    }
+
+
+  //**************************************************************************
+  //** getUploadDir
+  //**************************************************************************
+    private static javaxt.io.Directory getUploadDir(){
+        JSONObject config = Config.get("webserver").toJSONObject();
+        javaxt.io.Directory jobDir = null;
+        if (config.has("jobDir")){
+            String dir = config.get("jobDir").toString().trim();
+            if (dir.length()>0){
+                jobDir = new javaxt.io.Directory(dir);
+                jobDir = new javaxt.io.Directory(jobDir.toString() + "uploads");
+                jobDir.create();
+            }
+        }
+        if (jobDir==null || !jobDir.exists()){
+            throw new IllegalArgumentException("Invalid \"jobDir\" defined in the \"webserver\" section of the config file");
+        }
+        return jobDir;
     }
 
 }
