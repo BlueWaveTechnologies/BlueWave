1dc4bf3808885a93ddb9b62d5bd4d535dc3eb3ec
Peter Borissow <peter.borissow@yahoo.com>
2021-12-05 11:19:26 -0500

    Updated property names in the Imports_By_Country query

--- src/main/java/bluewave/queries/Imports_By_Country.sql
+++ src/main/java/bluewave/queries/Imports_By_Country.sql
@@ -3,5 +3,5 @@ WHERE n.country_of_origin IN[{country}]
 RETURN
 n.{establishment} as fei,
 count(n.{establishment}) as num_entries,
-COLLECT(n.product_code+"/"+n.Value+"/"+n.Quantity) as quantities
+COLLECT(n.product_code+"/"+n.value+"/"+n.quantity) as quantities
 order by num_entries desc
