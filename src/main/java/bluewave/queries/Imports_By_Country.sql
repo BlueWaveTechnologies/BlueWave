0907ae9ee08b6f4689f9441fa331d7f16ad8ce16
Peter Borissow <peter.borissow@yahoo.com>
2021-12-08 18:50:36 -0500

    Updated the Imports_By_Country query

--- src/main/java/bluewave/queries/Imports_By_Country.sql
+++ src/main/java/bluewave/queries/Imports_By_Country.sql
@@ -6,6 +6,13 @@ n.{establishment} as fei,
 count(n.{establishment}) as num_entries,
 sum(toFloat(n.quantity)) as quantity,
 sum(toFloat(n.value)) as value,
+
+collect(distinct(n.manufacturer)) as manufacturer,
+collect(distinct(n.shipper)) as shipper,
+collect(distinct(n.importer)) as importer,
+collect(distinct(n.consignee)) as consignee,
+collect(distinct(n.dii)) as dii,
+
 count(n.lab_classification_code) as num_exams,
 sum((CASE WHEN n.activity_number='23' THEN 1 ELSE 0 END)) as num_field_exams,
 sum((CASE WHEN (n.activity_number='23' AND n.lab_classification_code='3') THEN 1 ELSE 0 END)) as num_field_fails,
