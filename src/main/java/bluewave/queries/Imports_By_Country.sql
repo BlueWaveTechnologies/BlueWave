a4d894b46a153f2dbb3cd281621691f0d11c3879
Peter Borissow <peter.borissow@yahoo.com>
2021-12-10 08:14:46 -0500

    Updated Imports_By_Country query

--- src/main/java/bluewave/queries/Imports_By_Country.sql
+++ src/main/java/bluewave/queries/Imports_By_Country.sql
@@ -18,5 +18,6 @@ sum((CASE WHEN n.activity_number='23' THEN 1 ELSE 0 END)) as num_field_exams,
 sum((CASE WHEN (n.activity_number='23' AND n.lab_classification_code='3') THEN 1 ELSE 0 END)) as num_field_fails,
 sum((CASE WHEN n.activity_number='27' THEN 1 ELSE 0 END)) as num_label_exams,
 sum((CASE WHEN (n.activity_number='27' AND n.lab_classification_code='3') THEN 1 ELSE 0 END)) as num_label_fails,
+sum((CASE WHEN (n.sample_lab_classification='3') THEN 1 ELSE 0 END)) as num_bad_samples,
 sum((CASE WHEN toFloat(n.predict_risk)>=95 THEN 1 ELSE 0 END)) as num_hi_predict
 order by num_entries desc
