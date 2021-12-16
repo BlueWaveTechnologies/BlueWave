67dfc514ba2478dc66a50cdc4eaa23fb46bedd6c
Peter Borissow <peter.borissow@yahoo.com>
2021-12-16 05:19:18 -0500

    Renames entries to lines in Imports_Per_Day query

--- src/main/java/bluewave/queries/Imports_Per_Day.sql
+++ src/main/java/bluewave/queries/Imports_Per_Day.sql
@@ -3,6 +3,6 @@ WHERE n.country_of_origin IN[{country}]
 AND toFloat(n.predict_risk)>={threshold}
 RETURN
 n.date as date,
-count(n.date) as entries,
+count(n.date) as lines,
 sum(toFloat(n.quantity)) as quantity,
 sum(toFloat(n.value)) as value
