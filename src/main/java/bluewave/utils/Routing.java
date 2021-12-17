1c781ce4d9cb82680f7cc3aee83f8d7d1a1f1c2b
Peter Borissow <peter.borissow@yahoo.com>
2021-12-17 04:11:42 -0500

    Initial commit of the routing class

---
+++ src/main/java/bluewave/utils/Routing.java
@@ -0,0 +1,101 @@
+package bluewave.utils;
+
+import java.util.*;
+import java.math.BigDecimal;
+
+import javaxt.json.JSONArray;
+import javaxt.json.JSONObject;
+
+import org.gavaghan.geodesy.*;
+
+//******************************************************************************
+//**  Routing
+//******************************************************************************
+/**
+ *   Provides static methods used to calculate routes on the earth
+ *
+ ******************************************************************************/
+
+public class Routing {
+
+    private static GeodeticCalculator geoCalc = new GeodeticCalculator();
+    private static Ellipsoid wgs84 = Ellipsoid.WGS84;
+
+  //**************************************************************************
+  //** getGreatCircleRoute
+  //**************************************************************************
+    public static JSONObject getGreatCircleRoute(BigDecimal[] start, BigDecimal[] end, int numPoints) throws Exception {
+
+        GlobalCoordinates c1 = new GlobalCoordinates(start[0].doubleValue(), start[1].doubleValue());
+        GlobalCoordinates c2 = new GlobalCoordinates(end[0].doubleValue(), end[1].doubleValue());
+
+        GeodeticCurve geoCurve = geoCalc.calculateGeodeticCurve(wgs84, c1, c2);
+        double distance = geoCurve.getEllipsoidalDistance(); //meters
+        double segmentLength = distance/(double) numPoints;
+        double bearing = geoCurve.getAzimuth();
+
+        JSONObject properties = new JSONObject();
+        properties.set("distance", distance);
+        properties.set("bearing", bearing);
+
+        ArrayList<Double[]> coords = new ArrayList<>();
+        coords.add(new Double[]{c1.getLongitude(),c1.getLatitude()});
+
+        GlobalCoordinates prevPoint = c1;
+
+        for (int i=0; i<numPoints; i++){
+
+            GlobalCoordinates c = geoCalc.calculateEndingGlobalCoordinates(wgs84, prevPoint, bearing, segmentLength);
+            geoCurve = geoCalc.calculateGeodeticCurve(wgs84, c, c2);
+            bearing = geoCurve.getAzimuth();
+            prevPoint = c;
+
+            coords.add(new Double[]{c.getLongitude(),c.getLatitude()});
+        }
+
+        coords.add(new Double[]{c2.getLongitude(),c2.getLatitude()});
+
+        JSONObject geometry = new JSONObject();
+        geometry.set("type", "LineString");
+        JSONArray coordinates = new JSONArray();
+        geometry.set("coordinates", coordinates);
+        for (Double[] coord : coords){
+            JSONArray coordinate = new JSONArray();
+            coordinate.add(coord[0]);
+            coordinate.add(coord[1]);
+            coordinates.add(coordinate);
+        }
+
+        JSONObject geoJSON = new JSONObject();
+
+        geoJSON.set("geometry", geometry);
+        geoJSON.set("properties", properties);
+
+        return geoJSON;
+    }
+
+  //**************************************************************************
+  //** getShippingRoute
+  //**************************************************************************
+    public static JSONObject getShippingRoute(BigDecimal[] start, BigDecimal[] end, String shippingMethod) throws Exception {
+      //Get script
+        javaxt.io.File[] scripts = Python.getScriptDir().getFiles("shipment_route.py", true);
+        if (scripts.length==0) throw new Exception("Script not found");
+
+
+      //Compile command line options
+        ArrayList<String> params = new ArrayList<>();
+        params.add("-o="+start[0]+","+start[1]);
+        params.add("-d="+end[0]+","+end[1]);
+        params.add("--entrymode="+shippingMethod);
+
+
+      //Execute script
+        JSONObject geoJSON = Python.executeScript(scripts[0], params);
+
+
+
+        return geoJSON;
+    }
+
+}
