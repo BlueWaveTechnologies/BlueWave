588410850b96d874e094e700189eee8ac1f15c8d
Peter Borissow <peter.borissow@yahoo.com>
2022-01-28 13:14:54 -0500

    Expanded application domain models

---
+++ src/main/java/bluewave/app/Document.java
@@ -0,0 +1,196 @@
+package bluewave.app;
+import javaxt.json.*;
+import java.sql.SQLException;
+
+
+//******************************************************************************
+//**  Document Class
+//******************************************************************************
+/**
+ *   Used to represent a Document
+ *
+ ******************************************************************************/
+
+public class Document extends javaxt.sql.Model {
+
+    private String title;
+    private String description;
+    private File file;
+    private Integer pageCount;
+    private String indexStatus;
+    private JSONObject info;
+
+
+  //**************************************************************************
+  //** Constructor
+  //**************************************************************************
+    public Document(){
+        super("application.document", java.util.Map.ofEntries(
+            
+            java.util.Map.entry("title", "title"),
+            java.util.Map.entry("description", "description"),
+            java.util.Map.entry("file", "file_id"),
+            java.util.Map.entry("pageCount", "page_count"),
+            java.util.Map.entry("indexStatus", "index_status"),
+            java.util.Map.entry("info", "info")
+
+        ));
+        
+    }
+
+
+  //**************************************************************************
+  //** Constructor
+  //**************************************************************************
+  /** Creates a new instance of this class using a record ID in the database.
+   */
+    public Document(long id) throws SQLException {
+        this();
+        init(id);
+    }
+
+
+  //**************************************************************************
+  //** Constructor
+  //**************************************************************************
+  /** Creates a new instance of this class using a JSON representation of a
+   *  Document.
+   */
+    public Document(JSONObject json){
+        this();
+        update(json);
+    }
+
+
+  //**************************************************************************
+  //** update
+  //**************************************************************************
+  /** Used to update attributes using a record in the database.
+   */
+    protected void update(Object rs) throws SQLException {
+
+        try{
+            this.id = getValue(rs, "id").toLong();
+            this.title = getValue(rs, "title").toString();
+            this.description = getValue(rs, "description").toString();
+            Long fileID = getValue(rs, "file_id").toLong();
+            this.pageCount = getValue(rs, "page_count").toInteger();
+            this.indexStatus = getValue(rs, "index_status").toString();
+            this.info = new JSONObject(getValue(rs, "info").toString());
+
+
+
+          //Set file
+            if (fileID!=null) file = new File(fileID);
+
+        }
+        catch(Exception e){
+            if (e instanceof SQLException) throw (SQLException) e;
+            else throw new SQLException(e.getMessage());
+        }
+    }
+
+
+  //**************************************************************************
+  //** update
+  //**************************************************************************
+  /** Used to update attributes with attributes from another Document.
+   */
+    public void update(JSONObject json){
+
+        Long id = json.get("id").toLong();
+        if (id!=null && id>0) this.id = id;
+        this.title = json.get("title").toString();
+        this.description = json.get("description").toString();
+        if (json.has("file")){
+            file = new File(json.get("file").toJSONObject());
+        }
+        else if (json.has("fileID")){
+            try{
+                file = new File(json.get("fileID").toLong());
+            }
+            catch(Exception e){}
+        }
+        this.pageCount = json.get("pageCount").toInteger();
+        this.indexStatus = json.get("indexStatus").toString();
+        this.info = json.get("info").toJSONObject();
+    }
+
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public File getFile(){
+        return file;
+    }
+
+    public void setFile(File file){
+        this.file = file;
+    }
+
+    public Integer getPageCount(){
+        return pageCount;
+    }
+
+    public void setPageCount(Integer pageCount){
+        this.pageCount = pageCount;
+    }
+
+    public String getIndexStatus(){
+        return indexStatus;
+    }
+
+    public void setIndexStatus(String indexStatus){
+        this.indexStatus = indexStatus;
+    }
+
+    public JSONObject getInfo(){
+        return info;
+    }
+
+    public void setInfo(JSONObject info){
+        this.info = info;
+    }
+    
+    
+
+
+  //**************************************************************************
+  //** get
+  //**************************************************************************
+  /** Used to find a Document using a given set of constraints. Example:
+   *  Document obj = Document.get("title=", title);
+   */
+    public static Document get(Object...args) throws SQLException {
+        Object obj = _get(Document.class, args);
+        return obj==null ? null : (Document) obj;
+    }
+
+
+  //**************************************************************************
+  //** find
+  //**************************************************************************
+  /** Used to find Documents using a given set of constraints.
+   */
+    public static Document[] find(Object...args) throws SQLException {
+        Object[] obj = _find(Document.class, args);
+        Document[] arr = new Document[obj.length];
+        for (int i=0; i<arr.length; i++){
+            arr[i] = (Document) obj[i];
+        }
+        return arr;
+    }
+}
---
+++ src/main/java/bluewave/app/File.java
@@ -0,0 +1,196 @@
+package bluewave.app;
+import javaxt.json.*;
+import java.sql.SQLException;
+import javaxt.utils.Date;
+
+//******************************************************************************
+//**  File Class
+//******************************************************************************
+/**
+ *   Used to represent a File
+ *
+ ******************************************************************************/
+
+public class File extends javaxt.sql.Model {
+
+    private String name;
+    private Path path;
+    private String type;
+    private Date date;
+    private Long size;
+    private JSONObject metadata;
+
+
+  //**************************************************************************
+  //** Constructor
+  //**************************************************************************
+    public File(){
+        super("application.file", java.util.Map.ofEntries(
+            
+            java.util.Map.entry("name", "name"),
+            java.util.Map.entry("path", "path_id"),
+            java.util.Map.entry("type", "type"),
+            java.util.Map.entry("date", "date"),
+            java.util.Map.entry("size", "size"),
+            java.util.Map.entry("metadata", "metadata")
+
+        ));
+        
+    }
+
+
+  //**************************************************************************
+  //** Constructor
+  //**************************************************************************
+  /** Creates a new instance of this class using a record ID in the database.
+   */
+    public File(long id) throws SQLException {
+        this();
+        init(id);
+    }
+
+
+  //**************************************************************************
+  //** Constructor
+  //**************************************************************************
+  /** Creates a new instance of this class using a JSON representation of a
+   *  File.
+   */
+    public File(JSONObject json){
+        this();
+        update(json);
+    }
+
+
+  //**************************************************************************
+  //** update
+  //**************************************************************************
+  /** Used to update attributes using a record in the database.
+   */
+    protected void update(Object rs) throws SQLException {
+
+        try{
+            this.id = getValue(rs, "id").toLong();
+            this.name = getValue(rs, "name").toString();
+            Long pathID = getValue(rs, "path_id").toLong();
+            this.type = getValue(rs, "type").toString();
+            this.date = getValue(rs, "date").toDate();
+            this.size = getValue(rs, "size").toLong();
+            this.metadata = new JSONObject(getValue(rs, "metadata").toString());
+
+
+
+          //Set path
+            if (pathID!=null) path = new Path(pathID);
+
+        }
+        catch(Exception e){
+            if (e instanceof SQLException) throw (SQLException) e;
+            else throw new SQLException(e.getMessage());
+        }
+    }
+
+
+  //**************************************************************************
+  //** update
+  //**************************************************************************
+  /** Used to update attributes with attributes from another File.
+   */
+    public void update(JSONObject json){
+
+        Long id = json.get("id").toLong();
+        if (id!=null && id>0) this.id = id;
+        this.name = json.get("name").toString();
+        if (json.has("path")){
+            path = new Path(json.get("path").toJSONObject());
+        }
+        else if (json.has("pathID")){
+            try{
+                path = new Path(json.get("pathID").toLong());
+            }
+            catch(Exception e){}
+        }
+        this.type = json.get("type").toString();
+        this.date = json.get("date").toDate();
+        this.size = json.get("size").toLong();
+        this.metadata = json.get("metadata").toJSONObject();
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Path getPath(){
+        return path;
+    }
+
+    public void setPath(Path path){
+        this.path = path;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
+    public void setDate(Date date){
+        this.date = date;
+    }
+
+    public Long getSize(){
+        return size;
+    }
+
+    public void setSize(Long size){
+        this.size = size;
+    }
+
+    public JSONObject getMetadata(){
+        return metadata;
+    }
+
+    public void setMetadata(JSONObject metadata){
+        this.metadata = metadata;
+    }
+    
+    
+
+
+  //**************************************************************************
+  //** get
+  //**************************************************************************
+  /** Used to find a File using a given set of constraints. Example:
+   *  File obj = File.get("name=", name);
+   */
+    public static File get(Object...args) throws SQLException {
+        Object obj = _get(File.class, args);
+        return obj==null ? null : (File) obj;
+    }
+
+
+  //**************************************************************************
+  //** find
+  //**************************************************************************
+  /** Used to find Files using a given set of constraints.
+   */
+    public static File[] find(Object...args) throws SQLException {
+        Object[] obj = _find(File.class, args);
+        File[] arr = new File[obj.length];
+        for (int i=0; i<arr.length; i++){
+            arr[i] = (File) obj[i];
+        }
+        return arr;
+    }
+}
