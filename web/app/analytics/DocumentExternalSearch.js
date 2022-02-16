0b52a76d344627886e86086f54b97ddecc559cd9
Kenneth <kennethmcquadesales@gmail.com>
2022-02-16 17:52:04 -0500

    initial commit External Search

--- web/app/analytics/DocumentExternalSearch.js
+++ web/app/analytics/DocumentExternalSearch.js
@@ -14,7 +14,7 @@ bluewave.analytics.DocumentExternalSearch = function(parent, config) {
     var me = this;
     var carousel;
     var resultsPanel;
-
+    var searchPopup;
     var waitmask;
     // var results = {};
 
@@ -54,8 +54,122 @@ bluewave.analytics.DocumentExternalSearch = function(parent, config) {
         td = document.createElement("td");
         tr.appendChild(td);
         createFooter(td);
+
+        createSearchPopup();
     };
 
+  //**************************************************************************
+  //** createWindow
+  //**************************************************************************
+    var createWindow = function(config){
+      var win = new javaxt.dhtml.Window(document.body, config);
+      // windows.push(win);
+      return win;
+    };
+
+  //**************************************************************************
+  //** createSearchPopup
+  //**************************************************************************
+    createSearchPopup = function(){
+
+
+        searchPopup = createWindow({
+            title: "Search Results",
+            width: 400,
+            height: 600,
+            valign: "top",
+            modal: true,
+            style: config.style.window
+        });
+
+        var createButton = function(parent, label){
+          var input = document.createElement('input');
+          input.className = "form-button";
+          input.type = "button";
+          input.name = label;
+          input.value = label;
+          input.disabled = true;
+          parent.appendChild(input);
+          return input;
+      };
+
+      var createButtonBar = function(parent){
+        // add container
+          var div = document.createElement("div");
+          div.style.textAlign = "center";
+          div.style.padding = "10px 10px 10px 0";
+          parent.appendChild(div);
+        // add search button
+          var button = createButton(div,"Expand Search");
+
+        button.enable = function(){
+          this.disabled = false;
+        };
+
+        button.disable = function(){
+          this.disabled = false;
+        };
+
+        button.enable();
+
+        button.onclick = function(){
+          console.log("button was clicked");
+        };
+      };
+
+
+      var createSearchResultMessage = function(parent){
+        // create container
+          var div = document.createElement("div");
+          div.className = "doc-external-search-popup-results";
+          div.style.textAlign = "center";
+        // add text
+          var text = document.createElement("div");
+          text.className = "doc-external-search-popup-results-text";
+          div.appendChild(text);
+          text.innerText =  "No Results returned - test message";
+        // add background
+          var iconContainer = document.createElement("div");
+          iconContainer.className = "doc-external-search-popup-results-background";
+          div.appendChild(iconContainer);
+          iconContainer.innerHTML ='<i class="fas fa-not-equal"></i>';
+
+        parent.appendChild(div);
+      };
+
+        var body = searchPopup.getBody();
+        var table = createTable();
+        var tbody = table.firstChild;
+
+        body.appendChild(table);
+        this.el = table;
+
+        var tr, td;
+
+        tr = document.createElement("tr");
+        tbody.appendChild(tr);
+        td = document.createElement("td");
+        td.style.height = "100%";
+        tr.appendChild(td);
+        createSearchResultMessage(td);
+
+        tr = document.createElement("tr");
+        tbody.appendChild(tr);
+        td = document.createElement("td");
+        tr.appendChild(td);
+        createButtonBar(td);
+
+        searchPopup.hide();
+        // searchPopup.show();
+    };
+
+
+  //**************************************************************************
+  //** getSearchPopup
+  //**************************************************************************
+    this.showSearchPopup = function(){
+      searchPopup.show();
+    };
 
   //**************************************************************************
   //** getResultsPanel
@@ -139,16 +253,6 @@ bluewave.analytics.DocumentExternalSearch = function(parent, config) {
         // div.innerHTML = '<i class="fas fa-not-equal"></i>';
         td.appendChild(div);
 
-        var createButton = function(label){
-          var input = document.createElement('input');
-          input.className = "form-button";
-          input.type = "button";
-          input.name = label;
-          input.value = label;
-          input.disabled = true;
-          div.appendChild(input);
-          return input;
-      };
 
         return {
             el: el,
