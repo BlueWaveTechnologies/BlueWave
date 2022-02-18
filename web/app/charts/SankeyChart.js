498e28b6e82dfe5192b4c323312e1820a7ae56cd
Peter Borissow <peter.borissow@yahoo.com>
2022-02-18 09:02:11 -0500

    Added new public methods to the SankeyChart

--- web/app/charts/SankeyChart.js
+++ web/app/charts/SankeyChart.js
@@ -24,7 +24,9 @@ bluewave.charts.SankeyChart = function(parent, config) {
             opacity: 0.3
         }
     };
-    var svg, sankeyArea;
+    var svg, chart, sankeyArea;
+    var nodes = [];
+    var links = [];
 
 
   //**************************************************************************
@@ -32,14 +34,62 @@ bluewave.charts.SankeyChart = function(parent, config) {
   //**************************************************************************
     var init = function(){
 
-        config = merge(config, defaultConfig);
-
+        me.setConfig(config);
 
         initChart(parent, function(s, g){
             svg = s;
-            sankeyArea = g;
+            chart = g;
         });
+    };
 
+
+  //**************************************************************************
+  //** setConfig
+  //**************************************************************************
+    this.setConfig = function(chartConfig){
+        if (!chartConfig) config = defaultConfig;
+        else config = merge(chartConfig, defaultConfig);
+    };
+
+
+  //**************************************************************************
+  //** getNodeLabel
+  //**************************************************************************
+    this.getNodeLabel = function(d){
+        return d.name;
+    };
+
+
+  //**************************************************************************
+  //** addNode
+  //**************************************************************************
+    this.addNode = function(name, group){
+        if (!name) return;
+        var node;
+        if (name.name) node = name;
+        else node = {
+            name: name,
+            group: group
+        };
+        nodes.push(node);
+        return node;
+    };
+
+
+  //**************************************************************************
+  //** addLink
+  //**************************************************************************
+    this.addLink = function(source, target, value){
+        if (!source || !target) return;
+        if (source.name) source = source.name;
+        if (target.name) target = target.name;
+        var link = {
+            source: source,
+            target: target,
+            value: value
+        };
+        links.push(link);
+        return link;
     };
 
 
@@ -47,7 +97,9 @@ bluewave.charts.SankeyChart = function(parent, config) {
   //** clear
   //**************************************************************************
     this.clear = function(){
-        sankeyArea.selectAll("*").remove();
+        clearChart();
+        nodes = [];
+        links = [];
     };
 
 
@@ -55,160 +107,187 @@ bluewave.charts.SankeyChart = function(parent, config) {
   //** update
   //**************************************************************************
     this.update = function(chartConfig, data){
-        me.clear();
+        //me.clear();
+        me.setConfig(chartConfig);
 
-        if (!data) return;
-
-        if (chartConfig){
-            if (chartConfig.margin) config.margin = chartConfig.margin;
-            if (chartConfig.links) config.links = chartConfig.links;
+        if (data){
+            me.clear();
+            if (data.nodes){
+                nodes = data.nodes;
+            }
+            if (data.links){
+                links = data.links;
+            }
         }
 
 
-        var margin = config.margin;
-        sankeyArea.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        onRender(svg.node().parentNode, function(){
-
-            var width = parent.offsetWidth;
-            var height = parent.offsetHeight;
-            svg.attr("width", width);
-            svg.attr("height", height);
-
-
-
-          //Update width and height for the sankey
-            width = width - margin.left - margin.right;
-            height = height - margin.top - margin.bottom;
-
-            var sankey = d3
-              .sankey()
-              .nodeId((d) => d.name)
-              .nodeWidth(20)
-              .nodePadding(20)
-              .iterations([6])
-              .size([width, height]);
-
-
-
-            let graph = sankey(data);
-            var size = sankey.size();
-            var width = size[0];
-
-            var formatNumber = d3.format(",.0f"); // zero decimal places
-
-
-          //Create function to colorize categorical data
-            var getColor = d3.scaleOrdinal(d3.schemeCategory10);
-
-
-
-          //Add the links
-            var link = sankeyArea
-              .append("g")
-              .selectAll(".link")
-              .data(graph.links)
-              .enter()
-              .append("path")
-              .attr("class", "sankey-link")
-              .attr("d", d3.sankeyLinkHorizontal())
-              .attr("stroke-width", function (d) {
-                return d.width;
-              })
-              .style("stroke-opacity", function (d) {
-                return (d.opacity=config.links.opacity);
-              })
-              .style("stroke", function (d) {
-                  return config.links.color;
-              })
-              .on('mouseover', function(d){
-                  var opacity = Math.min(1, config.links.opacity*1.3);
-                  d3.select(this).style("stroke-opacity", opacity);
-              })
-              .on('mouseout', function(d){
-                d3.select(this).style("stroke-opacity", d.opacity);
-              });
-
-
-
-          //Add the nodes
-            var node = sankeyArea
-              .append("g")
-              .selectAll(".node")
-              .data(graph.nodes)
-              .enter()
-              .append("g")
-              .attr("class", "sankey-node");
-
-
-          //Add the rectangles for the nodes
-            node
-              .append("rect")
-              .attr("x", function (d) {
-                return d.x0;
-              })
-              .attr("y", function (d) {
-                return d.y0;
-              })
-              .attr("height", function (d) {
-                return d.y1 - d.y0;
-              })
-              .attr("width", sankey.nodeWidth())
-              .style("fill", function (d) {
-                return (d.color = getColor(d.name.replace(/ .*/, "")));
-              })
-              .style("stroke", function (d) {
-                return d3.rgb(d.color).darker(2);
-              })
-              .append("title")
-              .text(function (d) {
-                return d.name + "\n" + formatNumber(d.value);
-              });
-
-
-
-
-          //Update link color AFTER node color is set
-            if (config.links.color==="source"){
-                link.each(function() {
-                    var path = d3.select(this);
-                    path.style("stroke", function (d) {
-                        return d.source.color;
-                    });
-                });
-            }
-
-
-
-          //Add link labels
-            link.append("title").text(function (d) {
-              return d.source.name + " → " + d.target.name + "\n" + formatNumber(d.value);
-            });
-
-
-
-          //Add node labels
-            node
-              .append("text")
-              .attr("x", function (d) {
-                return d.x0 - 6;
-              })
-              .attr("y", function (d) {
-                return (d.y1 + d.y0) / 2;
-              })
-              .attr("dy", "0.35em")
-              .attr("text-anchor", "end")
-              .text(function (d) {
-                return d.name;
-              })
-              .filter(function (d) {
-                return d.x0 < width / 2;
-              })
-              .attr("x", function (d) {
-                return d.x1 + 6;
-              })
-              .attr("text-anchor", "start");
+        var parent = svg.node().parentNode;
+        onRender(parent, function(){
+            renderChart(parent);
         });
+    };
+
+
+  //**************************************************************************
+  //** clearChart
+  //**************************************************************************
+    var clearChart = function(){
+        if (chart) chart.selectAll("*").remove();
+    };
+
+
+  //**************************************************************************
+  //** renderChart
+  //**************************************************************************
+    var renderChart = function(parent){
+        clearChart();
+
+
+        var width = parent.offsetWidth;
+        var height = parent.offsetHeight;
+        sankeyArea = chart.append("g");
+        sankeyArea
+            .attr("name", "sankey")
+            .attr("width", width)
+            .attr("height", height);
+
+
+
+      //Create sankey
+        var sankey = d3
+          .sankey()
+          .nodeId((d) => d.name)
+          .nodeWidth(20)
+          .nodePadding(20)
+          .iterations([6])
+          .size([width, height]);
+
+
+      //Add nodes and links to the sankey
+        let graph = sankey({
+            nodes: nodes,
+            links: links
+        });
+
+
+        var size = sankey.size();
+        var width = size[0];
+
+        var formatNumber = d3.format(",.0f"); // zero decimal places
+
+
+      //Create function to colorize categorical data
+        var getColor = d3.scaleOrdinal(d3.schemeCategory10);
+
+
+
+      //Add the links
+        var linkGroup = sankeyArea.append("g");
+        linkGroup.attr("name", "links");
+
+        var link = linkGroup.selectAll("*")
+          .data(graph.links)
+          .enter()
+          .append("path")
+          .attr("class", "sankey-link")
+          .attr("d", d3.sankeyLinkHorizontal())
+          .attr("stroke-width", function (d) {
+            return Math.max(d.width, 1);
+          })
+          .style("stroke-opacity", function (d) {
+            return (d.opacity=config.links.opacity);
+          })
+          .style("stroke", function (d) {
+              return config.links.color;
+          })
+          .on('mouseover', function(d){
+              var opacity = Math.min(1, config.links.opacity*1.3);
+              d3.select(this).style("stroke-opacity", opacity);
+          })
+          .on('mouseout', function(d){
+            d3.select(this).style("stroke-opacity", d.opacity);
+          });
+
+
+
+      //Add the nodes
+        var nodeGroup = sankeyArea.append("g");
+        nodeGroup.attr("name", "nodes");
+
+        var node = nodeGroup.selectAll("*")
+          .data(graph.nodes)
+          .enter()
+          .append("g")
+          .attr("class", "sankey-node");
+
+
+      //Add the rectangles for the nodes
+        node
+          .append("rect")
+          .attr("x", function (d) {
+            return d.x0;
+          })
+          .attr("y", function (d) {
+            return d.y0;
+          })
+          .attr("height", function (d) {
+              return Math.max(d.y1 - d.y0, 1);
+          })
+          .attr("width", sankey.nodeWidth())
+          .style("fill", function (d) {
+            return (d.color = getColor(d.name.replace(/ .*/, "")));
+          })
+          .style("stroke", function (d) {
+            return d3.rgb(d.color).darker(2);
+          })
+          .append("title")
+          .text(function (d) {
+            return d.name + "\n" + formatNumber(d.value);
+          });
+
+
+
+
+      //Update link color AFTER node color is set
+        if (config.links.color==="source"){
+            link.each(function() {
+                var path = d3.select(this);
+                path.style("stroke", function (d) {
+                    return d.source.color;
+                });
+            });
+        }
+
+
+
+      //Add link labels
+        link.append("title").text(function (d) {
+          return d.source.name + " → " + d.target.name + "\n" + formatNumber(d.value);
+        });
+
+
+
+      //Add node labels
+        node
+          .append("text")
+          .attr("x", function (d) {
+            return d.x0 - 6;
+          })
+          .attr("y", function (d) {
+            return (d.y1 + d.y0) / 2;
+          })
+          .attr("dy", "0.35em")
+          .attr("text-anchor", "end")
+          .text(function (d) {
+              return me.getNodeLabel(d);
+          })
+          .filter(function (d) {
+            return d.x0 < width / 2;
+          })
+          .attr("x", function (d) {
+            return d.x1 + 6;
+          })
+          .attr("text-anchor", "start");
     };
 
 
