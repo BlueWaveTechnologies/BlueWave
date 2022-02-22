6e3cb84a58f6ff22bc94ac0a7b012de972e3743a
Kenneth <kennethmcquadesales@gmail.com>
2022-02-22 14:23:56 -0500

    refactor documentExternalSearch - add base datagrid
    main points of refactor are to replace carousel with a panel that can be shown within document analysis, and remove noResultsPanel from this file & related functions

--- web/app/analytics/DocumentExternalSearch.js
+++ web/app/analytics/DocumentExternalSearch.js
@@ -12,11 +12,8 @@ if(!bluewave.analytics) bluewave.analytics={};
 bluewave.analytics.DocumentExternalSearch = function(parent, config) {
 
     var me = this;
-    var carousel;
-    var resultsPanel;
-    var searchPopup;
     var waitmask;
-    // var results = {};
+    var grid;
 
 
 
@@ -49,146 +46,22 @@ bluewave.analytics.DocumentExternalSearch = function(parent, config) {
         createBody(td);
 
 
-        tr = document.createElement("tr");
-        tbody.appendChild(tr);
-        td = document.createElement("td");
-        tr.appendChild(td);
-        createFooter(td);
-
-        createSearchPopup();
     };
 
   //**************************************************************************
-  //** createWindow
+  //** getDataGrid
   //**************************************************************************
-    var createWindow = function(config){
-      var win = new javaxt.dhtml.Window(document.body, config);
-      // windows.push(win);
-      return win;
+    this.getDataGrid = function(){
+      return grid;
     };
 
-  //**************************************************************************
-  //** createSearchPopup
-  //**************************************************************************
-    createSearchPopup = function(){
-
-
-        searchPopup = createWindow({
-            title: "Search Results",
-            width: 400,
-            height: 600,
-            valign: "top",
-            modal: true,
-            style: config.style.window
-        });
-
-        var createButton = function(parent, label){
-          var input = document.createElement('input');
-          input.className = "form-button";
-          input.type = "button";
-          input.name = label;
-          input.value = label;
-          input.disabled = true;
-          parent.appendChild(input);
-          return input;
-      };
-
-      var createButtonBar = function(parent){
-        // add container
-          var div = document.createElement("div");
-          div.style.textAlign = "center";
-          div.style.padding = "10px 10px 10px 0";
-          parent.appendChild(div);
-        // add search button
-          var button = createButton(div,"Expand Search");
-
-        button.enable = function(){
-          this.disabled = false;
-        };
-
-        button.disable = function(){
-          this.disabled = false;
-        };
-
-        button.enable();
-
-        button.onclick = function(){
-          console.log("button was clicked");
-        };
-      };
-
-
-      var createSearchResultMessage = function(parent){
-        // create container
-          var div = document.createElement("div");
-          div.className = "doc-external-search-popup-results";
-          div.style.textAlign = "center";
-        // add text
-          var text = document.createElement("div");
-          text.className = "doc-external-search-popup-results-text";
-          div.appendChild(text);
-          text.innerText =  "No Results returned - test message";
-        // add background
-          var iconContainer = document.createElement("div");
-          iconContainer.className = "doc-external-search-popup-results-background";
-          div.appendChild(iconContainer);
-          iconContainer.innerHTML ='<i class="fas fa-not-equal"></i>';
-
-        parent.appendChild(div);
-      };
-
-        var body = searchPopup.getBody();
-        var table = createTable();
-        var tbody = table.firstChild;
-
-        body.appendChild(table);
-        this.el = table;
-
-        var tr, td;
-
-        tr = document.createElement("tr");
-        tbody.appendChild(tr);
-        td = document.createElement("td");
-        td.style.height = "100%";
-        tr.appendChild(td);
-        createSearchResultMessage(td);
-
-        tr = document.createElement("tr");
-        tbody.appendChild(tr);
-        td = document.createElement("td");
-        tr.appendChild(td);
-        createButtonBar(td);
-
-        searchPopup.hide();
-        // searchPopup.show();
-    };
-
-
-  //**************************************************************************
-  //** getSearchPopup
-  //**************************************************************************
-    this.showSearchPopup = function(){
-      searchPopup.show();
-    };
-
-  //**************************************************************************
-  //** getResultsPanel
-  //**************************************************************************
-    this.getResultsPanel = function(){
-        return resultsPanel.el;
-    };
 
 
   //**************************************************************************
   //** clear
   //**************************************************************************
     this.clear = function(){
-        var panels = carousel.getPanels();
-        for (var i=0; i<panels.length; i++){
-            var panel = panels[i];
-            var el = panel.div.firstChild;
-            if (el) panel.div.removeChild(el);
-        }
+
     };
 
 
@@ -204,54 +77,64 @@ bluewave.analytics.DocumentExternalSearch = function(parent, config) {
   //**************************************************************************
   //** update
   //**************************************************************************
-  /** Used to populate the panels in the carousel control. Assumes that the
-   *  carousel is cleared (see clear method)
-   */
     var update = function(){
-        var panels = carousel.getPanels();
-        if (!resultsPanel) resultsPanel = createResultsPanel();
-        resultsPanel.update();
-        panels[0].div.appendChild(resultsPanel.el);
+      grid.update();
     };
 
+
   //**************************************************************************
-  //** createResultsPanel
+  //** downloadFile
   //**************************************************************************
-    var createResultsPanel = function(){
+  //* left empty for rewrite by instantiator (documentAnalysis)
+  this.downloadFile = function(){
 
-        var table, tbody, tr, td;
-
-      //Create main table
-        table = createTable();
-        tbody = table.firstChild;
-        var el = table;
-
-      //Create title
-        tr = document.createElement("tr");
-        tbody.appendChild(tr);
-        td = document.createElement("td");
-        // td.className = "doc-external-search-title";
-        tr.appendChild(td);
-        var icon = document.createElement("i");
-        td.appendChild(icon);
-        var title = document.createElement("span");
-        td.appendChild(title);
+  };
 
 
-      //Create body
-        tr = document.createElement("tr");
-        tbody.appendChild(tr);
-        td = document.createElement("td");
-        td.style.height = "100%";
-        td.style.verticalAlign = "top";
-        td.style.padding = "20px 5px 0";
-        tr.appendChild(td);
+  //**************************************************************************
+  //** createBody
+  //**************************************************************************
+    var createBody = function(parent){
+
+      var table = createTable();
+      var tbody = table.firstChild;
+      parent.appendChild(table);
+      var el = table;
+      var tr, td;
+
+      // add a place to insert search bar when this page is active
+          tr = document.createElement("tr");
+          tbody.appendChild(tr);
+          td = document.createElement("td");
+          td.className = "document-search-search-bar";
 
 
-        var div = document.createElement("div");
-        // div.className = "doc-external-search-background";
-        // div.innerHTML = '<i class="fas fa-not-equal"></i>';
-        td.appendChild(div);
+          tr.appendChild(td);
+
+          td.style.height = "30px"; // temporary styling
+          td.style.paddingTop ="2px"; // temporary styling
+          td.style.paddingBottom ="2px"; // temporary styling
+          td.style.paddingLeft ="4px"; // temporary styling
+          td.style.paddingRight ="4px"; // temporary styling
+
+      // add ExternalSearchResults page body below search bar
+          tr = document.createElement("tr");
+          tbody.appendChild(tr);
+          td = document.createElement("td");
+          tr.appendChild(td);
+          table = createTable();
+          tbody = table.firstChild;
+          td.appendChild(table);
+
+          tr = document.createElement("tr");
+          tbody.appendChild(tr);
+          td = document.createElement("td");
+          td.style.height = "100%";
+          td.style.verticalAlign = "top";
+          td.style.padding = "20px 5px 0";
+          tr.appendChild(td);
+          createGrid(td);
+
 
 
         return {
@@ -262,57 +145,74 @@ bluewave.analytics.DocumentExternalSearch = function(parent, config) {
         };
     };
 
-
   //**************************************************************************
-  //** createBody
+  //** createGrid
   //**************************************************************************
-    var createBody = function(parent){
+  var createGrid = function(parent){
+    var df = d3.timeFormat(getDateFormat(config.dateFormat));
+    var columnConfig = [
+        {header: 'Name', width:'100%', field: 'name', sortable: true},
+        {header: 'Date', width:'150', field: 'date', sortable: true, align:'right'},
+        {header: 'Type', width:'140', field: 'type', sortable: true},
+        {header: 'Size', width:'115', field: 'size', sortable: true, align:'right'}
+    ];
+    if (config.showCheckboxes===true){
+        columnConfig.unshift({header: 'x', field: 'id', width:30});
+    }
+    if (typeof IScroll !== 'undefined'){
+        columnConfig.push({header: '', width: 8});
+    }
 
+    var params = {};
+    grid = new javaxt.dhtml.DataGrid(parent, {
+        columns: columnConfig,
+        style: config.style.table,
+        url: "/documents", // update this to reflect the Javaxt url for external searches
+        params: params,
+        parseResponse: function(request){ // maybe update this
+            var csv = request.responseText;
+            var rows = parseCSV(csv, ",");
+            var header = rows.shift();
+            var createRecord = function(row){
+                var r = {};
+                header.forEach((field, i)=>{
+                    var v = row[i];
+                    if (field=="id" || field=="size"){
+                        v = parseFloat(v);
+                    }
+                    else if (field=="info"){
+                        if (v) v = JSON.parse(decodeURIComponent(v));
+                    }
+                    r[field] = v;
+                });
+                return r;
+            };
 
-      //Create carousel
-        carousel = new javaxt.dhtml.Carousel(parent, {
-            drag: false,
-            loop: true,
-            animate: true,
-            animationSteps: 600,
-            transitionEffect: "easeInOutCubic",
-            fx: config.fx
-        });
+            var data = [];
+            rows.forEach((row)=>{
+                data.push(createRecord(row));
+            });
 
-        onRender(carousel.el, ()=>{
-            console.log("ready!");
-        });
+            return data;
+        },
+        update: function(row, record){
 
+        }
+    });
 
+    grid.onBeforeLoad = function(){
+        waitmask.show();
+    };
 
-      //Create a panel for the carousel
-        var panel = document.createElement("div");
-        panel.style.height = "100%";
-        carousel.add(panel);
-
-
-      //Add event handlers
-        carousel.onChange = function(){
-
-        };
-
+    grid.onLoad = function(){
+        waitmask.hide();
     };
 
 
-  //**************************************************************************
-  //** createFooter
-  //**************************************************************************
-    var createFooter = function(parent){
-
-        var div = document.createElement("div");
-        div.className = "noselect";
-        div.style.float = "right";
-        div.style.textAlign = "center";
-        div.style.padding = "10px 10px 10px 0";
-        parent.appendChild(div);
+    grid.update = function(q){
 
     };
-
+  };
 
   //**************************************************************************
   //** Utils
@@ -323,6 +223,7 @@ bluewave.analytics.DocumentExternalSearch = function(parent, config) {
     // var isArray = javaxt.dhtml.utils.isArray;
     // var round = javaxt.dhtml.utils.round;
     // var get = bluewave.utils.get;
+    var getDateFormat = bluewave.chart.utils.getDateFormat;
 
 
     init();
