fe0811831d24740637e8da7239479e9088e5753f
Peter Borissow <peter.borissow@yahoo.com>
2022-03-02 18:17:00 -0500

    Updated dbView to use new json output from the GraphService in Explorer

--- web/app/Explorer.js
+++ web/app/Explorer.js
@@ -1666,23 +1666,13 @@ bluewave.Explorer = function(parent, config) {
                         success: function(nodes){
                             waitmask.hide();
 
-                          //Parse response
-                            var arr = [];
-                            for (var i=0; i<nodes.length; i++){
-                                var nodeName = nodes[i].node;
-                                if (nodeName){
-                                    arr.push({
-                                        name: nodes[i].node,
-                                        properties: nodes[i].properties
-                                    });
-                                }
-                            }
-                            arr.sort(function(a, b){
+                          //Sort nodes alphabetically
+                            nodes.sort(function(a, b){
                                 return a.name.localeCompare(b.name);
                             });
 
                           //Add nodes to the tree
-                            tree.addNodes(arr);
+                            tree.addNodes(nodes);
                         },
                         failure: function(request){
                             if (waitmask) waitmask.hide();
