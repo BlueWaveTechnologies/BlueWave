b0854d1f7274128a50ca5f92af981aa245aadbb5
Peter <peter.borissow@yahoo.com>
2022-05-16 06:43:09 -0400

    Added new getExtensions() method to the Plugin class

--- src/main/java/bluewave/Plugin.java
+++ src/main/java/bluewave/Plugin.java
@@ -1,9 +1,10 @@
 package bluewave;
 import java.util.*;
+import javaxt.json.*;
 
 //XML parser for plugins
 import static javaxt.xml.DOM.*;
-import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.*;
 import org.w3c.dom.Node;
 
 public class Plugin {
@@ -12,6 +13,9 @@ public class Plugin {
     private javaxt.io.Directory dir;
     private String name;
 
+  //**************************************************************************
+  //** Constructor
+  //**************************************************************************
     public Plugin(javaxt.io.File xmlFile){
         xml = xmlFile.getXML();
         dir = xmlFile.getDirectory();
@@ -21,15 +25,27 @@ public class Plugin {
             name = getAttributeValue(attr, "name");
         }
     }
-
+    
+    
+  //**************************************************************************
+  //** getName
+  //**************************************************************************
     public String getName(){
         return name;
     }
-
+    
+    
+  //**************************************************************************
+  //** getDirectory
+  //**************************************************************************
     public javaxt.io.Directory getDirectory(){
         return dir;
     }
 
+    
+  //**************************************************************************
+  //** getWebServices
+  //**************************************************************************
     public HashMap<String, String> getWebServices(){
         HashMap<String, String> webservices = new HashMap<>();
         for (Node node : getElementsByTagName("webservices", xml)){
@@ -42,6 +58,59 @@ public class Plugin {
         }
         return webservices;
     }
-
     
+    
+  //**************************************************************************
+  //** getExtensions
+  //**************************************************************************
+  /** Returns a json object representing all the web extension 
+   */
+    public JSONObject getExtensions(){
+
+        JSONObject extensions = new JSONObject();
+        
+        for (Node ext : getElementsByTagName("extensions", xml)){
+            for (Node node : getNodes(ext.getChildNodes())){
+
+              //Get component
+                String component = node.getNodeName();
+                JSONArray extension = extensions.get(component).toJSONArray();
+                if (extension==null){
+                    extension = new JSONArray();
+                    extensions.set(component, extension);
+                }
+                
+                
+              //Get config
+                JSONObject json = new JSONObject();
+                String type = getAttributeValue(node, "type");
+                json.set("type", type);
+                for (Node n : getNodes(node.getChildNodes())){
+                    json.set("config", getJson(n));
+                }
+
+                extension.add(json);
+            }
+        }
+        
+        return extensions;
+    }
+    
+    
+    private JSONObject getJson(Node node){
+        JSONObject json = new JSONObject();
+        
+        NamedNodeMap attributes = node.getAttributes();
+        for (int i=0; i<attributes.getLength(); i++){
+            Node attr = attributes.item(i);
+            json.set(attr.getNodeName(), attr.getNodeValue());
+        }
+        
+        for (Node n : getNodes(node.getChildNodes())){
+            //if (n.getNodeType()!=1) continue; 
+            String nodeName = n.getNodeName();
+            json.set(nodeName, getJson(n));
+        }
+        return json;
+    }
 }
