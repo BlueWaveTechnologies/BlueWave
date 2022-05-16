b6bde1df852070fede02f9cca8a668ff5a987b92
Peter Borissow <peter.borissow@yahoo.com>
2022-05-16 14:28:44 -0400

    Added logic to append plugin includes to main.html in the WebApp

--- src/main/java/bluewave/web/WebApp.java
+++ src/main/java/bluewave/web/WebApp.java
@@ -11,6 +11,9 @@ import static javaxt.utils.Console.*;
 
 import bluewave.utils.NotificationService;
 
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 //******************************************************************************
 //**  WebApp
 //******************************************************************************
@@ -277,6 +280,28 @@ public class WebApp extends HttpServlet {
             }
             else{
 
+
+              //Special case for plugins
+                for (bluewave.Plugin plugin : Config.getPlugins()){
+                    StringBuilder pluginPath = new StringBuilder(
+                    plugin.getDirectory().toString().replace("\\", "/") + "web");
+
+                    for (String p : path.replace("\\", "/").split("/")){
+                        if (p.equals(".") || p.equals("..")){
+                            break;
+                        }
+                        pluginPath.append("/" + p);
+                    }
+
+                    javaxt.io.File f = new javaxt.io.File(pluginPath.toString());
+                    if (f.exists()){
+                        fileManager.sendFile(f, request, response);
+                        return;
+                    }
+                }
+
+
+
               //Check if the service matches a file or folder in the web directory.
               //If so, send the static file as requested. Note that the current
               //implementation searches the web directory for each http request,
@@ -339,6 +364,10 @@ public class WebApp extends HttpServlet {
                 response.write("Unauthorized");
                 return;
             }
+
+
+            sendMain(path, request, response);
+            return;
         }
 
 
@@ -354,10 +383,100 @@ public class WebApp extends HttpServlet {
 
 
   //**************************************************************************
+  //** sendMain
+  //**************************************************************************
+  /** Used to send the main.html file to the client. Appends any plugin
+   *  includes as needed.
+   */
+    private void sendMain(String path, HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+
+
+      //Get all the includes associated with the plugins
+        ArrayList<Node> includes = new ArrayList<>();
+        for (bluewave.Plugin plugin : Config.getPlugins()){
+            for (Node node : plugin.getIncludes()){
+                includes.add(node);
+            }
+        }
+
+
+
+        if (includes.isEmpty()){
+            sendFile(path, request, response);
+        }
+        else{
+
+          //fileManager
+            javaxt.io.File htmlFile = new javaxt.io.File(web, "main.html");
+
+
+
+          //Convert html to xml. Assumes the html file is xhtml. Note that the
+          //parser will be extremely slow if there is a !DOCTYPE declaration.
+            String xhtml = htmlFile.getText().trim();
+            int idx = xhtml.toUpperCase().indexOf("<!DOCTYPE");
+            if (idx>-1){
+                xhtml = xhtml.substring(idx+"<!DOCTYPE".length()).trim();
+                xhtml = xhtml.substring(xhtml.indexOf(">")+1).trim();
+            }
+            org.w3c.dom.Document xml = javaxt.xml.DOM.createDocument(xhtml);
+
+
+            try{
+
+              //Add includes
+                Node head = javaxt.xml.DOM.getElementsByTagName("head", xml)[0];
+                for (Node include : includes){
+                    Node n = xml.importNode(include, true);
+                    head.appendChild(n);
+                }
+
+
+              //Update links to scripts and css files
+                long lastUpdate = fileManager.updateLinks(htmlFile, xml);
+
+
+
+
+              //Replace all self enclosing tags
+                Node outerNode = javaxt.xml.DOM.getOuterNode(xml);
+                NodeList nodeList = outerNode.getChildNodes();
+                for (int i=0; i<nodeList.getLength(); i++){
+                    Node node = nodeList.item(i);
+                    String nodeName = node.getNodeName().toLowerCase();
+                    if (nodeName.equals("head") || nodeName.equals("body")){
+                        fileManager.updateNodes(node.getChildNodes(), xml);
+                    }
+                }
+
+
+              //Convert xml to string
+                String html = javaxt.xml.DOM.getText(xml);
+                html = html.replace("<!-- -->", ""); //replace empty comments
+                html = html.substring(html.indexOf(">")+1); //remove xml header
+
+
+              //Set content type and send response
+                response.setContentType("text/html");
+                fileManager.sendResponse(html, lastUpdate, request, response);
+
+            }
+            catch(Exception e){
+                response.setStatus(500);
+                response.write("Internal server error: " + e.getMessage());
+            }
+
+        }
+    }
+
+
+  //**************************************************************************
   //** getRequestHeaders
   //**************************************************************************
-  /** Used to convert http request headers and metadata into a string. Updates
-   *  the "Authorization" header to hide passwords.
+  /** Used to convert http request headers and metadata into a string for
+   *  logging purposes. Updates the "Authorization" header to hide passwords.
    */
     private String getRequestHeaders(HttpServletRequest request) {
 
