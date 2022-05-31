310ade698f8ce37f74472e0122dbd74408124669
Peter Borissow <peter.borissow@yahoo.com>
2022-05-31 11:38:18 -0400

    Minor style and nav updates in ConfigAdmin

--- web/app/admin/ConfigAdmin.js
+++ web/app/admin/ConfigAdmin.js
@@ -18,7 +18,8 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
     var waitmask;
     var carousel;
     var panels = [];
-    
+    var subtitle;
+
   //**************************************************************************
   //** Constructor
   //**************************************************************************
@@ -35,14 +36,14 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
         var table = createTable();
         var tbody = table.firstChild;
         var tr, td;
-        
+
         tr = document.createElement("tr");
         tbody.appendChild(tr);
         td = document.createElement("td");
         createHeader(td);
         tr.appendChild(td);
-        
-        
+
+
         tr = document.createElement("tr");
         tbody.appendChild(tr);
         td = document.createElement("td");
@@ -51,8 +52,8 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
         td.style.verticalAlign = "top";
         createBody(td);
         tr.appendChild(td);
-        
-        
+
+
         parent.appendChild(table);
         me.el = table;
         addShowHide(me);
@@ -73,8 +74,8 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
     this.update = function(){
 
     };
-    
-    
+
+
   //**************************************************************************
   //** createHeader
   //**************************************************************************
@@ -85,26 +86,38 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
 
         var icon = document.createElement("div");
         icon.className = "fas fa-sliders-h";
+        icon.onclick = function(){
+            subtitle.innerHTML = "";
+            carousel.back();
+        };
         div.appendChild(icon);
 
         var title = document.createElement("div");
         title.innerHTML = "General Settings";
+        title.onclick = function(){
+            subtitle.innerHTML = "";
+            carousel.back();
+        };
         div.appendChild(title);
+
+        subtitle = document.createElement("div");
+        subtitle.className = "admin-subheader noselect";
+        div.appendChild(subtitle);
     };
-    
-    
+
+
   //**************************************************************************
   //** createBody
   //**************************************************************************
     var createBody = function(parent){
-        
+
       //Create container
         var div = document.createElement("div");
         div.className = "";
         div.style.display = "inline";
         parent.appendChild(div);
-        
-        
+
+
       //Create carousel
         carousel = new javaxt.dhtml.Carousel(div, {
             drag: false,
@@ -114,7 +127,8 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
             transitionEffect: "easeInOutCubic",
             fx: config.fx
         });
-        
+
+
       //Create 2 panels for the carousel
         for (var i=0; i<2; i++){
             var panel = document.createElement("div");
@@ -123,23 +137,13 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
         }
 
 
-      //Add event handlers
-        carousel.beforeChange = function(){
-            //me.setTitle("");
-            //backButton.hide();
-            //nextButton.hide();
-        };
-
-
-        carousel.onChange = function(currPanel){
-            console.log(currPanel);
-        };
-        
-        
-        
+      //Get first panel
         var mainDiv = carousel.getPanels()[0].div;
-        
-        
+
+        var div = document.createElement("div");
+        div.className = "admin-config-table";
+        mainDiv.appendChild(div);
+
         var table = createTable();
         table.style.height = "";
         var tbody = table.firstChild;
@@ -150,14 +154,14 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
         td = document.createElement("td");
         td.style.width = "36px";
         tr.appendChild(td);
-        
+
         td = document.createElement("td");
         td.style.width = "100%";
         tr.appendChild(td);
-        
-        
+
+
         addRow(tbody, {
-            icon: "browserIcon",
+            icon: "far fa-compass",
             title: "Supported Browsers",
             onClick: function(){
                 raisePanel(bluewave.admin.BrowserAdmin, config);
@@ -165,40 +169,43 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
         });
 
 
-        mainDiv.appendChild(table);
+        div.appendChild(table);
     };
-    
-    
+
+
   //**************************************************************************
   //** addRow
   //**************************************************************************
     var addRow = function(parent, config){
-        
+
         var tr = document.createElement("tr");
-        tr.className = "admin-config-row noselect";
+        tr.className = "admin-config-row admin-config-row-next noselect";
         parent.appendChild(tr);
         var td;
 
 
         td = document.createElement("td");
-        
         tr.appendChild(td);
-        
+        var div = document.createElement("div");
+        div.className = config.icon;
+        td.appendChild(div);
+
+
         td = document.createElement("td");
         td.innerHTML = config.title;
         tr.appendChild(td);
-        
+
         tr.onclick = function(){
             if (config.onClick) config.onClick();
         };
     };
-    
-    
+
+
   //**************************************************************************
   //** raisePanel
   //**************************************************************************
     var raisePanel = function(cls, config){
-        
+
         var p;
         panels.forEach((panel)=>{
             if (panel instanceof cls){
@@ -208,8 +215,8 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
                 panel.hide();
             }
         });
-        
-        
+
+
         if (p){
             p.show();
         }
@@ -220,6 +227,7 @@ bluewave.admin.ConfigAdmin = function(parent, config) {
         }
         p.update();
         carousel.next();
+        if (p.getTitle) subtitle.innerHTML = p.getTitle();
     };
 
 
