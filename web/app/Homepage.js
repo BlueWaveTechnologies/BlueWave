1267f1b53d09c43801d4cd0006c9b14f0e9612b0
Peter Borissow <peter.borissow@yahoo.com>
2022-07-14 15:09:25 -0400

    Updated the add() method in the Homepage to use the new dashboard preview panel

--- web/app/Homepage.js
+++ web/app/Homepage.js
@@ -12,7 +12,6 @@ bluewave.Homepage = function(parent, config) {
 
     var me = this;
     var mainDiv;
-    var t = new Date().getTime();
     var dashboardItems = [];
     var dashboardMenu, groupMenu; //callouts
     var waitmask;
@@ -352,16 +351,12 @@ bluewave.Homepage = function(parent, config) {
   //** add
   //**************************************************************************
     var add = function(dashboard, parent){
-        var title = dashboard.name;
 
 
       //Create dashboardItem
-        var dashboardItem = createDashboardItem(parent, {
-            width: 360,
-            height: 230,
-            //subtitle: title,
-            settings: true
-        });
+        var preview = new bluewave.dashboard.Preview(parent);
+        preview.update(dashboard);
+        var dashboardItem = preview.getDashboardItem();
         dashboardItem.dashboard = dashboard;
         dashboardItems.push(dashboardItem);
 
@@ -439,106 +434,6 @@ bluewave.Homepage = function(parent, config) {
                 });
             }
         };
-
-
-        var imageContainer = document.createElement("div");
-        imageContainer.className = "dashboard-item-image";
-        dashboardItem.innerDiv.appendChild(imageContainer);
-
-        var icon = document.createElement("i");
-        icon.className = "fas fa-camera";
-        imageContainer.appendChild(icon);
-
-
-        var img = document.createElement("img");
-        img.className = "noselect";
-        img.style.cursor = "pointer";
-        img.style.opacity = 0;
-        img.onload = function() {
-            imageContainer.innerHTML = "";
-            imageContainer.style.backgroundImage = "url(dashboard/thumbnail?id=" + dashboard.id + "&_=" + t + ")";
-            if (true) return;
-
-            var rect = javaxt.dhtml.utils.getRect(imageContainer);
-            imageContainer.appendChild(this);
-
-
-            var maxWidth = rect.width;
-            var maxHeight = rect.height;
-            var width = 0;
-            var height = 0;
-
-            var setWidth = function(){
-                var ratio = maxWidth/width;
-                width = width*ratio;
-                height = height*ratio;
-            };
-
-            var setHeight = function(){
-                var ratio = maxHeight/height;
-                width = width*ratio;
-                height = height*ratio;
-            };
-
-
-            var resize = function(img){
-                width = img.width;
-                height = img.height;
-
-                if (maxHeight<maxWidth){
-
-                    setHeight();
-                    if (width>maxWidth) setWidth();
-                }
-                else{
-                    setWidth();
-                    if (height>maxHeight) setHeight();
-                }
-
-                if (width===0 || height===0) return;
-
-
-                //img.width = width;
-                //img.height = height;
-
-                //TODO: Insert image into a canvas and do a proper resize
-                //ctx.putImageData(img, 0, 0);
-                //resizeCanvas(canvas, width, height, true);
-                //var base64image = canvas.toDataURL("image/png");
-
-            };
-
-            resize(this);
-
-        };
-        img.src = "dashboard/thumbnail?id=" + dashboard.id + "&_=" + t;
-        imageContainer.style.backgroundImage = "url(dashboard/thumbnail?id=" + dashboard.id + "&_=" + t + ")";
-
-
-        var dashboardBody = document.createElement("div");
-        dashboardBody.className = "dashboard-item-body";
-        dashboardItem.innerDiv.appendChild(dashboardBody);
-
-        var dashboardTitle = document.createElement("div");
-        dashboardTitle.className = "dashboard-item-title";
-        dashboardTitle.innerText = dashboard.name;
-        dashboardBody.appendChild(dashboardTitle);
-
-
-        if (dashboard.info){
-            var description = dashboard.info.description;
-            if (description){
-                description.split(/\n\n/).forEach((d)=>{
-                    d = d.trim();
-                    if (d.length===0) return;
-                    var dashboardDescription = document.createElement("div");
-                    dashboardDescription.className = "dashboard-item-description";
-                    dashboardDescription.innerHTML = d;
-                    dashboardBody.appendChild(dashboardDescription);
-                });
-            }
-        }
-
     };
 
 
